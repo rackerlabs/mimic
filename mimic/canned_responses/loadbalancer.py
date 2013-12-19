c87b96977bdab0357272e0c2b198cea9a1a8cbcc
Lekha Jeevan <lekha.jeevan@rackspace.com>
2013-12-19 13:12:16 -0800

    lbaas mimic

---
+++ mimic/canned_responses/loadbalancer.py
@@ -0,0 +1,61 @@
+"""
+Canned response for add and delete node for load balancers
+"""
+from random import randrange
+from twisted.python import log
+
+
+lb_node_id_cache = {}
+
+
+def add_node(node_list, lb_id):
+    """
+    Returns the canned response for add nodes
+    """
+    node_response_list = []
+    if lb_id not in lb_node_id_cache:
+        lb_node_id_cache[lb_id] = {}
+    for node in node_list:
+        node_data = {}
+        node_data['id'] = str(randrange(99999))
+        node_data['status'] = 'ONLINE'
+        node_data['address'] = node['address']
+        node_data['port'] = node['port']
+        node_data['condition'] = node['condition']
+        try:
+            if node['weight']:
+                node_data['weight'] = node['weight']
+        except:
+            KeyError
+        try:
+            if node['type']:
+                node_data['type'] = node['type']
+        except:
+            KeyError
+        lb_node_id_cache[lb_id][node_data['id']] = node_data
+        node_response_list.append(node_data)
+    #log.msg(lb_node_id_cache)
+    return {'nodes': node_response_list}
+
+
+def delete_node(lb_id, node_id):
+    """
+    Determines whether the node to be deleted exists in mimic cache and
+    returns the response code.
+    """
+    response = 404
+    if lb_id in lb_node_id_cache:
+        if node_id in lb_node_id_cache[lb_id]:
+            del lb_node_id_cache[lb_id][node_id]
+            response = 204
+    return response
+
+
+def list_nodes(lb_id):
+    """
+    Returns the list of nodes remaining on the load balancer
+    """
+    if lb_id in lb_node_id_cache:
+        return lb_node_id_cache[lb_id].values()
+    else:
+        return []
---
+++ mimic/rest/loadbalancer_api.py
@@ -0,0 +1,59 @@
+"""
+Defines add node and delete node from load balancers
+"""
+
+import json
+from twisted.web.server import Request
+from mimic.canned_responses.loadbalancer import (add_node, delete_node, list_nodes)
+from mimic.rest.mimicapp import MimicApp
+
+
+Request.defaultContentType = 'application/json'
+failing_lb_id = '175647'
+_count = 11
+
+
+class LoadBalancerApi(object):
+
+    """
+    Rest endpoints for mocked Load balancer api.
+    """
+    app = MimicApp()
+
+    @app.route('/v2/<string:tenant_id>/loadbalancers/<string:lb_id>/nodes', methods=['POST'])
+    def add_node_to_load_balancer(self, request, tenant_id, lb_id):
+        """
+        Return a successful add node response
+        """
+        if str(lb_id) == failing_lb_id:
+            global _count
+            if _count != 0:
+                _count = _count - 1
+                request.setResponseCode(422)
+                return json.dumps({'message': "Load Balancer {0} has a status of 'PENDING_UPDATE' \
+                    and is considered immutable.".format(lb_id), 'code': 422})
+        content = json.loads(request.content.read())
+        node_list = content['nodes']
+        request.setResponseCode(200)
+        return json.dumps(add_node(node_list, lb_id))
+
+    @app.route('/v2/<string:tenant_id>/loadbalancers/<string:lb_id>/nodes/<string:node_id>',
+               methods=['DELETE'])
+    def delete_node_from_load_balancer(self, request, tenant_id, lb_id, node_id):
+        """
+        Returns a 204 response code, for any load balancer created using the mocks
+        """
+        # if str(lb_id) == failing_lb_id:
+        #     request.setResponseCode(422)
+        #     return json.dumps({'message': "Load Balancer {0} has a status of 'PENDING_UPDATE' \
+        #         and is considered immutable.".format(lb_id), 'code': 422})
+        return request.setResponseCode(delete_node(lb_id, node_id))
+
+    @app.route('/v2/<string:tenant_id>/loadbalancers/<string:lb_id>/nodes',
+               methods=['GET'])
+    def list_nodes_for_load_balancer(self, request, tenant_id, lb_id):
+        """
+        Returns a 200 response code and list of nodes on the load balancer
+        """
+        request.setResponseCode(200)
+        return json.dumps({"nodes": list_nodes(lb_id)})
