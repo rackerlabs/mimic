e7fac038e7c5e28b3035fb39a99a20e93fd01089
Lekha Jeevan <lekha.jeevan@rackspace.com>
2014-02-20 15:09:41 -0800

    review

--- mimic/canned_responses/loadbalancer.py
+++ mimic/canned_responses/loadbalancer.py
@@ -52,9 +52,9 @@ def load_balancer_example(lb_info, lb_id, status):
                   "connectionLogging": lb_info.get("connectionLogging", {"enabled": False}),
                   "contentCaching": {"enabled": False}}
     if lb_info.get("nodes"):
-        lb_example.update({"nodes": _add_nodes_to_lb(lb_info["nodes"])})
+        lb_example.update({"nodes": _format_nodes_on_lb(lb_info["nodes"])})
     if lb_info.get("metadata"):
-        lb_example.update({"metadata": _add_meta(lb_info["metadata"])})
+        lb_example.update({"metadata": _format_meta(lb_info["metadata"])})
     return lb_example
 
 
@@ -99,7 +99,7 @@ def add_node(node_list, lb_id):
     Returns the canned response for add nodes
     """
     if lb_id in lb_cache:
-        nodes = _add_nodes_to_lb(node_list)
+        nodes = _format_nodes_on_lb(node_list)
         if lb_cache[lb_id].get("nodes"):
             for existing_node in lb_cache[lb_id]["nodes"]:
                 for new_node in node_list:
@@ -144,7 +144,7 @@ def list_nodes(lb_id):
         return not_found_response("loadbalancer"), 404
 
 
-def _add_nodes_to_lb(node_list):
+def _format_nodes_on_lb(node_list):
     """
     create a dict of nodes given the list of nodes
     """
@@ -164,7 +164,7 @@ def _add_nodes_to_lb(node_list):
     return nodes
 
 
-def _add_meta(node_list):
+def _format_meta(node_list):
     """
     creates metadata with 'id' as a key
     """
