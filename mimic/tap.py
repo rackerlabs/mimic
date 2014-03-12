46e272ed173efbdc394c8bdc071083384439eb3f
Lekha Jeevan <lekha.jeevan@rackspace.com>
2014-03-12 15:33:02 -0700

    include-port-ip-options

--- mimic/tap.py
+++ mimic/tap.py
@@ -3,18 +3,32 @@ from twisted.application.service import MultiService
 from twisted.web.server import Site
 from mimic.rest import auth_api, nova_api, loadbalancer_api, mimic_api
 from twisted.python import usage
+from mimic.util.mimic_options import OPTION_VALUES
 
 
 class Options(usage.Options):
-    pass
+    """
+    Set the options
+    """
+    optParameters = [
+        ["port", "p", "8900",
+         "port for Mimic API connection. Note the mocked services use "
+         " increments of the given port in the order: Identity, Nova, LB"],
+        ["ip_address", "i", "localhost",
+         "Ip address on which the service is running."]
+    ]
 
 
 def makeService(config):
     """
-    Set up the otter-api service.
+    Set up the mimic-api service.
     """
     s = MultiService()
-    port_offset = 8900
+    options = dict(config)
+    port_offset = int(options['port'])
+    OPTION_VALUES['port'] = int(options['port'])
+    OPTION_VALUES['ip_address'] = options['ip_address']
+
     for klein_obj in (mimic_api.MimicPresetApi(), auth_api.AuthApi(),
                       nova_api.NovaApi(), loadbalancer_api.LoadBalancerApi()):
         site = Site(klein_obj.app.resource())
