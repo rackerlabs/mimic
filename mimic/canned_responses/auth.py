d1efdc6c29ce8c5cc21d419ccaeb148c8266aa28
Glyph <glyph@twistedmatrix.com>
2014-05-13 19:46:13 -0700

    Start refactoring service catalog somewhat.

--- mimic/canned_responses/auth.py
+++ mimic/canned_responses/auth.py
@@ -1,3 +1,4 @@
+# -*- test-case-name: mimic.test.test_auth -*-
 """
 Canned response for get auth token
 """
@@ -11,36 +12,118 @@ HARD_CODED_USER_ID = "10002"
 HARD_CODED_USER_NAME = "autoscaleaus"
 HARD_CODED_ROLES = [{"id": "1", "description": "Admin", "name": "Identity"}]
 
-def get_token(tenant_id):
+def format_timestamp(dt):
     """
-    Canned response for authentication, with service catalog containing endpoints only
-    for services implemented by Mimic.
+    Format the given timestamp.
+
+    :param datetime.datetime dt: A datetime.datetime object to be formatted.
     """
+    return dt.strftime('%Y-%m-%dT%H:%M:%S.999-05:00')
+
+
+
+class CatalogEndpoint(object):
+    def __init__(self, tenant_id, region):
+        self.tenant_id = tenant_id
+        self.region = region
+
+    def url_with_prefix(self, uri_prefix):
+        return uri_prefix + "/v2/" + self.tenant_id
+
+
+
+class CatalogEntry(object):
+    def __init__(self, tenant_id, type, name, regions):
+        self.type = type
+        self.tenant_id = tenant_id
+        self.name = name
+        self._regions = regions
+
+    @property
+    def endpoints(self):
+        """
+        Yield a list of CatalogEndpoint objects for each endpoint supported by
+        this catalog entry.
+        """
+        for region in self._regions:
+            yield CatalogEndpoint(self.tenant_id, region)
+
+    
+
+
+def canned_entries(tenant_id):
+    """
+    Some canned catalog entries.
+    """
+    return [
+        CatalogEntry(
+            tenant_id, "compute", "cloudServersOpenStack", ["ORD"]
+        ),
+        CatalogEntry(
+            tenant_id, "rax:load-balancer", "cloudLoadBalancers", ["ORD"]
+        ),
+    ]
+
+
+
+def get_token(tenant_id,
+              timestamp=format_timestamp,
+              entry_generator=canned_entries):
+    """
+    Canned response for authentication, with service catalog containing
+    endpoints only for services implemented by Mimic.
+
+    :param callable timestamp: A callable, like format_timestamp, which takes a
+        datetime and returns a string.
+    :param entry_generator: A callable, like canned_entries, which takes a
+        datetime and returns an iterable of CatalogEntry.
+
+    :return: a JSON-serializable dictionary matching the format of the JSON
+             response for the identity ``/v2/tokens`` request.
+    """
+    def entry_json():
+        # ugly hack corresponding to hard-coding in mimic.tap, eliminate as
+        # soon as that is gone.  note that the responsibility here is correct
+        # though; URI generation belongs in the auth system.
+        port_offset_by_service = {
+            "compute": 2,
+            "rax:load-balancer": 3,
+        }
+        for entry in entry_generator(tenant_id):
+            def endpoint_json():
+                for endpoint in entry.endpoints:
+                    yield {
+                        "region": endpoint.region,
+                        "tenantId": endpoint.tenant_id,
+                        "publicURL": endpoint.url_with_prefix(
+                            "http://localhost:{port}/".format(
+                                port=8900 + port_offset_by_service[entry.type]
+                            )
+                        ),
+                    }
+            yield {
+                "name": entry.name,
+                "type": entry.type,
+                "endpoints": list(endpoint_json())
+            }
+
     return {
         "access": {
             "token": {
                 "id": HARD_CODED_TOKEN,
-                "expires": ((datetime.now() + timedelta(1)).
-                            strftime(('%Y-%m-%dT%H:%M:%S.999-05:00'))),
+                "expires": timestamp(datetime.now() + timedelta(days=1)),
                 "tenant": {
                     "id": tenant_id,
                     "name": tenant_id},
                 "RAX-AUTH:authenticatedBy": ["PASSWORD"]},
-            "serviceCatalog": [
-                {"name": "cloudServersOpenStack",
-                 "endpoints": [{"region": "ORD",
-                                "tenantId": tenant_id,
-                                "publicURL": "http://localhost:8902/v2/{0}".format(tenant_id)}],
-                 "type": "compute"},
-                {"name": "cloudLoadBalancers",
-                 "endpoints": [{"region": "ORD",
-                                "tenantId": tenant_id,
-                                "publicURL": "http://localhost:8903/v2/{0}".format(tenant_id)}],
-                 "type": "rax:load-balancer"}],
-            "user": {"id": HARD_CODED_USER_ID,
-                     "name": HARD_CODED_USER_NAME,
-                     "roles": HARD_CODED_ROLES,
-                     }}}
+            "serviceCatalog": list(entry_json()),
+            "user": {
+                "id": HARD_CODED_USER_ID,
+                "name": HARD_CODED_USER_NAME,
+                "roles": HARD_CODED_ROLES,
+            }
+        }
+    }
 
 
 def get_user(tenant_id):
@@ -79,8 +162,8 @@ def get_user_token(expires_in, username):
 
 def get_endpoints(token_id):
     """
-    Canned response for Identity's get enpoints call. This returns endpoints only
-    for the services implemented by Mimic.
+    Canned response for Identity's get endpoints call.  This returns endpoints
+    only for the services implemented by Mimic.
     """
     if token_id in token_cache:
         tenant_id = token_cache[token_id]
