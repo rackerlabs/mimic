db8293222329482fe89cc68d704a24461db92c76
Glyph <glyph@twistedmatrix.com>
2014-05-29 15:05:33 -0700

    dependency updates

--- setup.py
+++ setup.py
@@ -11,6 +11,14 @@ setup(
     packages=find_packages(exclude=[]),
     package_data={'': ['LICENSE']},
     package_dir={'mimic': 'mimic'},
+    install_requires=[
+        "characteristic==0.1.0",
+        "klein==0.2.1",
+        "twisted>=13.2.0",
+        "jsonschema==2.0",
+        "treq",
+        "six",
+    ],
     include_package_data=True,
     license=open('LICENSE').read()
 )
