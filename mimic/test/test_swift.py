936be0775422f9185045f36008ecd3b4e62f4a9c
Glyph <glyph@twistedmatrix.com>
2014-07-09 16:50:17 -0700

    add create_container tests, refactor set-up to be used across both tests

--- mimic/test/test_swift.py
+++ mimic/test/test_swift.py
@@ -18,31 +18,56 @@ class SwiftTests(SynchronousTestCase):
     tests for swift API
     """
 
+    def setUp(self):
+        """
+        Set up to create the requests
+        """
+        self.core = MimicCore(Clock(), [SwiftMock()])
+        self.root = MimicRoot(self.core).app.resource()
+        self.response = request(
+            self, self.root, "POST", "/identity/v2.0/tokens",
+            dumps({
+                "auth": {
+                    "passwordCredentials": {
+                        "username": "test1",
+                        "password": "test1password",
+                    },
+                                   # TODO: should this really be 'tenantId'?
+                    "tenantName": "fun_tenant",
+                }
+        }))
+        self.auth_response = self.successResultOf(self.response)
+        self.json_body = self.successResultOf(
+            treq.json_content(self.auth_response))
+
     def test_service_catalog(self):
         """
         When provided with a :obj:`SwiftMock`, :obj:`MimicCore` yields a
         service catalog containing a swift endpoint.
         """
-        core = MimicCore(Clock(), [SwiftMock()])
-        root = MimicRoot(core).app.resource()
-        response = request(self, root, "POST", "/identity/v2.0/tokens",
-                           dumps({
-                               "auth": {
-                                   "passwordCredentials": {
-                                       "username": "test1",
-                                       "password": "test1password",
-                                   },
-                                   # TODO: should this really be 'tenantId'?
-                                   "tenantName": "fun_tenant",
-                               }
-                           }))
-        responseNow = self.successResultOf(response)
-        self.assertEqual(responseNow.code, 200)
-        jsonBody = self.successResultOf(treq.json_content(responseNow))
-        self.assertTrue(jsonBody)
-        sampleEntry = jsonBody['access']['serviceCatalog'][0]
-        self.assertEqual(sampleEntry['type'], u'object-store')
+        self.assertEqual(self.auth_response.code, 200)
+        self.assertTrue(self.json_body)
+        sample_entry = self.json_body['access']['serviceCatalog'][0]
+        self.assertEqual(sample_entry['type'], u'object-store')
+        sample_endpoint = sample_entry['endpoints'][0]
         self.assertEqual(
-            sampleEntry['endpoints'][0]['tenantId'],
+            sample_endpoint['tenantId'],
             normal_tenant_id_to_crazy_mosso_id("fun_tenant")
         )
+        self.assertEqual(sample_endpoint['region'], 'ORD')
+        self.assertEqual(len(self.json_body['access']['serviceCatalog']), 1)
+
+    def test_create_container(self):
+        """
+        Test to verify create container using :obj:`SwiftMock`
+        """
+        uri = (self.json_body['access']['serviceCatalog'][0]['endpoints'][0]
+               ['publicURL'] + '/testcontainer')
+        create_container = request(self, self.root, "PUT", uri)
+        create_container_response = self.successResultOf(create_container)
+        self.assertEqual(create_container_response.code, 201)
+        self.assertEqual(
+            self.successResultOf(treq.content(create_container_response)),
+            b"",
+        )
+
