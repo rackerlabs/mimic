df66ceec65794fb44578f4943492abcd775c819b
Lekha Jeevan <lekha.jeevan@rackspace.com>
2014-07-16 22:22:57 -0700

    tests for nova api's create, get, list and delete server calls

--- mimic/test/test_nova.py
+++ mimic/test/test_nova.py
@@ -1,13 +1,21 @@
 
 
 import itertools
-from json import dumps as dump_json
-from twisted.trial.unittest import TestCase
+import json
+import treq
+
+from twisted.trial.unittest import SynchronousTestCase
+from twisted.internet.task import Clock
 
 from mimic.canned_responses.nova import server_template
+from mimic.core import MimicCore
+from mimic.resource import MimicRoot
+from mimic.test.helpers import request
+from mimic.rest.nova_api import NovaApi
 
 
-class ResponseGenerationTests(TestCase):
+class ResponseGenerationTests(SynchronousTestCase):
+
     """
     Tests for Nova response generation.
     """
@@ -111,5 +119,133 @@ class ResponseGenerationTests(TestCase):
             "updated": "the_current_time",
             "user_id": "170454"
         }
-        self.assertEquals(dump_json(expectation, indent=2),
-                          dump_json(actual, indent=2))
+        self.assertEquals(json.dumps(expectation, indent=2),
+                          json.dumps(actual, indent=2))
+
+
+class NovaAPITests(SynchronousTestCase):
+
+    """
+    Tests for the Nova plugin api
+    """
+
+    def setUp(self):
+        """
+        Create a :obj:`MimicCore` with :obj:`NovaApi` as the only plugin,
+        and create a server
+        """
+        self.core = MimicCore(Clock(), [NovaApi()])
+        self.root = MimicRoot(self.core).app.resource()
+        self.response = request(
+            self, self.root, "POST", "/identity/v2.0/tokens",
+            json.dumps({
+                "auth": {
+                    "passwordCredentials": {
+                        "username": "test1",
+                        "password": "test1password",
+                    },
+                }
+            })
+        )
+        self.auth_response = self.successResultOf(self.response)
+        self.json_body = self.successResultOf(
+            treq.json_content(self.auth_response))
+        self.uri = (self.json_body['access']['serviceCatalog'][0]['endpoints'][0]['publicURL']
+                    + '/servers')
+        self.server_name = 'test_server'
+        self.create_server = request(
+            self, self.root, "POST", self.uri,
+            json.dumps({
+                "server": {
+                    "name": self.server_name,
+                    "imageRef": "test-image",
+                    "flavorRef": "test-flavor"
+                }
+            }))
+        self.create_server_response = self.successResultOf(self.create_server)
+        self.create_server_response_body = self.successResultOf(
+            treq.json_content(self.create_server_response))
+        self.server_id = self.create_server_response_body['server']['id']
+
+    def test_create_server(self):
+        """
+        Test to verify :func:`create_server` on ``POST /v2.0/<tenant_id>/servers``
+        """
+        self.assertEqual(self.create_server_response.code, 202)
+        self.assertTrue(type(self.server_id), unicode)
+
+    def test_list_servers(self):
+        """
+        Test to verify :func:`list_servers` on ``GET /v2.0/<tenant_id>/servers``
+        """
+        list_servers = request(self, self.root, "GET", self.uri)
+        list_servers_response = self.successResultOf(list_servers)
+        list_servers_response_body = self.successResultOf(
+            treq.json_content(list_servers_response))
+        self.assertEqual(list_servers_response.code, 200)
+        self.assertEqual(list_servers_response_body['servers'][0]['id'],
+                         self.server_id)
+        self.assertEqual(len(list_servers_response_body['servers']), 1)
+
+    def test_list_servers_with_args(self):
+        """
+        Test to verify :func:`list_servers` on ``GET /v2.0/<tenant_id>/servers?name<name>``,
+        when a server with that name exists
+        """
+        list_servers = request(self, self.root, "GET", self.uri + '?name=' + self.server_name)
+        list_servers_response = self.successResultOf(list_servers)
+        list_servers_response_body = self.successResultOf(
+            treq.json_content(list_servers_response))
+        self.assertEqual(list_servers_response.code, 200)
+        self.assertEqual(list_servers_response_body['servers'][0]['id'],
+                         self.server_id)
+        self.assertEqual(len(list_servers_response_body['servers']), 1)
+
+    def test_list_servers_with_args_negative(self):
+        """
+        Test to verify :func:`list_servers` on ``GET /v2.0/<tenant_id>/servers?name<name>``
+        when a server with that name does not exist
+        """
+        list_servers = request(self, self.root, "GET", self.uri + '?name=' + 'no_server')
+        list_servers_response = self.successResultOf(list_servers)
+        list_servers_response_body = self.successResultOf(
+            treq.json_content(list_servers_response))
+        self.assertEqual(list_servers_response.code, 200)
+        self.assertEqual(len(list_servers_response_body['servers']), 0)
+
+    def test_get_server(self):
+        """
+        Test to verify :func:`get_server` on ``GET /v2.0/<tenant_id>/servers/<server_id>``
+        """
+        get_server = request(self, self.root, "GET", self.uri + '/' + self.server_id)
+        get_server_response = self.successResultOf(get_server)
+        get_server_response_body = self.successResultOf(
+            treq.json_content(get_server_response))
+        self.assertEqual(get_server_response.code, 200)
+        self.assertEqual(get_server_response_body['server']['id'],
+                         self.server_id)
+        self.assertEqual(get_server_response_body['server']['status'], 'ACTIVE')
+
+    def test_list_servers_with_details(self):
+        """
+        Test to verify :func:`list_servers_with_details` on ``GET /v2.0/<tenant_id>/servers/detail``
+        """
+        list_servers_detail = request(self, self.root, "GET", self.uri + '/detail')
+        list_servers_detail_response = self.successResultOf(list_servers_detail)
+        list_servers_detail_response_body = self.successResultOf(
+            treq.json_content(list_servers_detail_response))
+        self.assertEqual(list_servers_detail_response.code, 200)
+        self.assertEqual(list_servers_detail_response_body['servers'][0]['id'],
+                         self.server_id)
+        self.assertEqual(len(list_servers_detail_response_body['servers']), 1)
+        self.assertEqual(list_servers_detail_response_body['servers'][0]['status'], 'ACTIVE')
+
+    def test_delete_server(self):
+        """
+        Test to verify :func:`delete_server` on ``DELETE /v2.0/<tenant_id>/servers/<server_id>``
+        """
+        delete_server = request(self, self.root, "DELETE", self.uri + '/' + self.server_id)
+        delete_server_response = self.successResultOf(delete_server)
+        self.assertEqual(delete_server_response.code, 204)
+        self.assertEqual(self.successResultOf(treq.content(delete_server_response)),
+                         b"")
