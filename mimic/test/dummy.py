9bbf21e7e8ca65a23714e84a186e2937c436e8f2
cyli <cyli@twistedmatrix.com>
2014-07-17 19:55:54 -0700

    Finish tests for service resources from the root resource

--- mimic/test/dummy.py
+++ mimic/test/dummy.py
@@ -5,11 +5,23 @@ Dummy classes that can be shared across test cases
 from zope.interface import implementer
 
 from twisted.plugin import IPlugin
-from twisted.web.resource import NoResource
+from twisted.web.resource import Resource
 
 from mimic.catalog import Entry
 from mimic.catalog import Endpoint
 from mimic.imimic import IAPIMock
+
+class ExampleResource(Resource):
+    """
+    Simple resource that returns a string as the response
+    """
+    isLeaf = True
+
+    def __init__(self, response_message):
+        self.response_message = response_message
+
+    def render_GET(self, request):
+        return self.response_message
 
 
 @implementer(IAPIMock, IPlugin)
@@ -17,12 +29,13 @@ class ExampleAPI(object):
     """
     Example API that returns NoResource
     """
-    def __init__(self):
+    def __init__(self, response_message="default message"):
         """
         Has a dictionary to store information from calls, for testing
         purposes
         """
         self.store = {}
+        self.response_message = response_message
 
     def catalog_entries(self, tenant_id):
         """
@@ -36,4 +49,4 @@ class ExampleAPI(object):
         Return no resource.
         """
         self.store['uri_prefix'] = uri_prefix
-        return NoResource()
+        return ExampleResource(self.response_message)
--- mimic/test/test_resource.py
+++ mimic/test/test_resource.py
@@ -1,20 +1,22 @@
-
 from twisted.internet.task import Clock
 from twisted.trial.unittest import SynchronousTestCase
 
 from mimic.core import MimicCore
 from mimic.resource import MimicRoot
 from mimic.test.dummy import ExampleAPI
-from mimic.test.helpers import request
+from mimic.test.helpers import request, request_with_content
 
 
-class PluginResourceTests(SynchronousTestCase):
+class ServiceResourceTests(SynchronousTestCase):
     """
-    Tests for ``/service/*`` endpoints.
+    Tests for ``/service/*`` endpoints, handled by
+    :func:`MimicRoot.get_service_resource`
     """
     def test_child_resource_gets_base_uri_from_request(self):
         """
-        A session is created for the token provided
+        Whatever the URI is used to access mimic is the one that is passed
+        back to the plugin when
+        :func:`mimic.imimic.IAPMock.resource_for_region` is called.
         """
         example = ExampleAPI()
 
@@ -33,3 +35,64 @@ class PluginResourceTests(SynchronousTestCase):
         self.assertEqual(
             "http://mybase/service/{0}/{1}/".format(region, service_id),
             example.store['uri_prefix'])
+
+    def test_service_endpoint_returns_404_if_wrong_service_id(self):
+        """
+        When the URI used to access a real service has the right region but
+        wrong service ID, a 404 is returned and the resource for the service
+        is accessed.
+        """
+        example = ExampleAPI()
+
+        core = MimicCore(Clock(), [example])
+        root = MimicRoot(core).app.resource()
+
+        # get the region and service id registered for the example API
+        (region, service_id) = core.uri_prefixes.keys()[0]
+
+        response = self.successResultOf(request(
+            self, root, "GET",
+            "http://mybase/service/{0}/not_{1}".format(region, service_id)
+        ))
+        self.assertEqual(404, response.code)
+        self.assertEqual([], example.store.keys())
+
+    def test_service_endpoint_returns_404_if_wrong_region(self):
+        """
+        When the URI used to access a real service has the right service ID
+        but wrong service ID, a 404 is returned and the resource for the
+        service is accessed.
+        """
+        example = ExampleAPI()
+
+        core = MimicCore(Clock(), [example])
+        root = MimicRoot(core).app.resource()
+
+        # get the region and service id registered for the example API
+        (region, service_id) = core.uri_prefixes.keys()[0]
+
+        response = self.successResultOf(request(
+            self, root, "GET",
+            "http://mybase/service/not_{0}/{1}".format(region, service_id)
+        ))
+        self.assertEqual(404, response.code)
+        self.assertEqual([], example.store.keys())
+
+    def test_service_endpoint_returns_service_resource(self):
+        """
+        When the URI used to access a real service has the right service ID
+        and right region, the service's resource is used to respond to the
+        request.
+        """
+        core = MimicCore(Clock(), [ExampleAPI('response!')])
+        root = MimicRoot(core).app.resource()
+
+        # get the region and service id registered for the example API
+        (region, service_id) = core.uri_prefixes.keys()[0]
+
+        (response, content) = self.successResultOf(request_with_content(
+            self, root, "GET",
+            "http://mybase/service/{0}/{1}".format(region, service_id)
+        ))
+        self.assertEqual(200, response.code)
+        self.assertEqual('response!', content)
