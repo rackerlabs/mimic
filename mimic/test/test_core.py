651ee343f7f9bac028f7aa66bea43d29cc3853a8
cyli <cyli@twistedmatrix.com>
2014-07-17 20:38:17 -0700

    Add core test coverage

--- mimic/test/test_core.py
+++ mimic/test/test_core.py
@@ -1,18 +1,19 @@
 
 from __future__ import unicode_literals
 
+from datetime import datetime
 import six
 
+from twisted.internet.task import Clock
 from twisted.trial.unittest import SynchronousTestCase
 
-from twisted.internet.task import Clock
 from mimic.core import MimicCore
-from datetime import datetime
+from mimic.plugins import nova_plugin, loadbalancer_plugin
 
 
 class SessionCreationTests(SynchronousTestCase):
     """
-    Tests for requesting sessions from a MimicCore.
+    Tests for requesting sessions from :class:`MimicCore`
     """
 
     def test_username_password_new(self):
@@ -123,3 +124,31 @@ class SessionCreationTests(SynchronousTestCase):
                                                      "sometenant")
         session2 = core.session_for_tenant_id("sometenant")
         self.assertIdentical(session, session2)
+
+
+class CoreBuildingTests(SynchronousTestCase):
+    """
+    Tests for creating a :class:`MimicCore` object with plugins
+    """
+    def test_no_uri_prefixes_if_no_plugins(self):
+        """
+        If there are no plugins provided to :class:`MimicCore`, there are no
+        uri prefixes or entries for the tenant.
+        """
+        core = MimicCore(Clock(), [])
+        self.assertEqual(0, len(core.uri_prefixes))
+        self.assertEqual([], list(core.entries_for_tenant('any_tenant', {},
+                                                          'http://mimic')))
+
+    def test_from_plugin_includes_all_plugins(self):
+        """
+        Using the :func:`MimicRoot.fromPlugin` creator for a
+        :class:`MimicCore`, the nova and loadbalancer plugins are included.
+        """
+        core = MimicCore.fromPlugins(Clock())
+        self.assertEqual(
+            set((nova_plugin.nova, loadbalancer_plugin.loadbalancer)),
+            set(core.uri_prefixes.values()))
+        self.assertEqual(
+            2, len(list(core.entries_for_tenant('any_tenant', {},
+                                                'http://mimic'))))
