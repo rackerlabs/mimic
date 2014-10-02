4ca6e9e6c6a41f1b5bdacbb0795f4d3a285275dd
Glyph <glyph@twistedmatrix.com>
2014-10-01 17:17:21 -0700

    how many seconds in a day

--- mimic/test/test_helper.py
+++ mimic/test/test_helper.py
@@ -4,6 +4,11 @@ from unittest import TestCase
 from datetime import datetime
 from mimic.util.helper import current_time_in_utc, fmt
 
+def no_total_seconds_in_26(td):
+    """
+    There's no timedelta.total_seconds in Python 2.6.
+    """
+    return td.seconds + (td.days * 24 * 3600)
 
 class TimeFormatTests(TestCase):
     """
@@ -20,6 +25,6 @@ class TimeFormatTests(TestCase):
         # Asserting about real time is tricky, so let's just say that it took
         # less than a minute to get from the first function call to the second
         # one.
-        delta = (close_to_now - parsed).total_seconds()
+        delta = no_total_seconds_in_26(close_to_now - parsed)
         self.assertTrue(abs(delta) < fudge_factor,
                         str(delta) + " was greater than a minute")
