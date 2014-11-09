41182e641700b8912751fbfbd8230e00b2d79e09
derwolfe <chriswwolfe@gmail.com>
2014-11-09 16:00:06 -0600

    Fix requirements for py2app
    There look to be a few outstanding bugs in py2app and its dependencies. I found the combination of dependencies that work here: http://aodj.ghost.io/isolation-in-practice-part-2/.
    
    The process I am using to build is:
    
    1) create a virtualenv and install mimic, it's dependencies, and the dependencies from the py2app-requires.txt file.
    2) run python setup-mac.py py2app.
    
    This is yieled an application, but it is crashing. I'm pretty sure the issues are related to either my start-app.py script being faulty or py2app not picking up a dependency.

--- start-app.py
+++ start-app.py
@@ -4,14 +4,12 @@ start_app.py starts the application for py2app.
 This calls twistd with default arguments.
 """
 
-from os.path import join, dirname
+from mimic.tap import Options, makeService
 from sys import argv
 from twisted.scripts.twistd import run
 
-# pass the name of the application - this fills argv.
 argv[1:] = [
     'mimic'
 ]
 
-# this will start the application
 run()
