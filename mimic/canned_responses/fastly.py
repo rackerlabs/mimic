bb25b4013bbcf114574e46e60846f86c6ef04137
Malini Kamalambal <malini.kamalambal@rackspace.com>
2014-11-13 15:48:23 -0500

    Mimic Fastly endpoints

--- mimic/canned_responses/fastly.py
+++ mimic/canned_responses/fastly.py
@@ -1,10 +1,37 @@
-# -*- test-case-name: mimic.test.test_loadbalancer -*-
 """
 Canned response for fastly
 """
 
+import uuid
+from characteristic import attributes, Attribute
+
 fastly_cache = {}
-meta_cache = {}
+
+
+@attributes(["name", "content_type", "data"])
+class Object(object):
+    """
+    A Python object (i.e. instance) representing a Service object (i.e. bag of
+    octets).
+    """
+
+    def as_json(self):
+        """
+        Create a JSON-serializable representation of the contents of this
+        object.
+        """
+        return {
+            "name": self.name,
+            "content_type": self.content_type,
+            "bytes": len(self.data),
+        }
+
+
+@attributes(["name", Attribute("objects", default_factory=dict)])
+class Service(object):
+    """
+    A Fastly Service (collection of :obj:`Object`.)
+    """
 
 
 def get_current_customer(request):
@@ -32,3 +59,123 @@ def get_current_customer(request):
         u'can_stream_syslog': u'1', u'pricing_plan': u'developer',
         u'billing_contact_id': None, u'has_streaming': u'1'}
     return current_customer, 200
+
+
+def create_service(request, url_data):
+    """
+    Returns POST service with response json.
+
+    :return: a JSON-serializable dictionary matching the format of the JSON
+             response for fastly_client.create_service()
+             ("/service") request.
+    """
+
+    data = dict((key, value) for key, value in url_data)
+    data = {
+        'customer_id': data['customer_id'][0],
+        'name': data['name'][0]}
+
+    publish_key = uuid.uuid4().hex
+    service_id = uuid.uuid4().hex
+    service_name = data['name']
+
+    global fastly_cache
+    fastly_cache[service_name] = {
+        'service_details': {
+            u'comment': '', u'locked': False,
+            u'updated_at': u'2014-11-13T14:29:10+00:00',
+            u'created_at': u'2014-11-13T14:29:10+00:00',
+            u'testing': None, u'number': 1, u'staging': None, u'active': None,
+            u'service_id': service_id, u'deleted_at': None,
+            u'inherit_service_id': None, u'deployed': None}
+    }
+    fastly_cache[service_id] = fastly_cache[service_name]
+
+    create_service = {
+        u'comment': '',
+        u'publish_key': publish_key,
+        u'name': service_name,
+        u'versions': [{u'comment': '', u'locked': u'0',
+                       u'service': service_id,
+                       u'updated_at': u'2014-11-12T18:43:21',
+                       u'created_at': u'2014-11-12T18:43:21',
+                       u'testing': None, u'number': u'1', u'staging': None,
+                       u'active': None,
+                       u'service_id': service_id,
+                       u'deleted_at': None, u'inherit_service_id': None,
+                       u'deployed': None, u'backend': 0}],
+        u'created_at': u'2014-11-12T18:43:21+00:00',
+        u'updated_at': u'2014-11-12T18:43:21+00:00',
+        u'customer_id': data['customer_id'],
+        u'id': service_id}
+    return create_service
+
+
+def get_service_by_name(request, service_name):
+    """Returns service details json.
+
+    :return: a JSON-serializable dictionary matching the format of the JSON
+             response for fastly_client.get_service_by_name()
+             ("/service/version") request.
+    """
+    global fastly_cache
+    return fastly_cache[service_name]
+
+
+def create_version(request, service_id):
+    """
+    Returns POST service with response json.
+
+    :return: a JSON-serializable dictionary matching the format of the JSON
+             response for fastly_client.create_version()
+             ("/service/version") request.
+    """
+    version = 2
+    create_version = {
+        'service_id': service_id,
+        'version': version}
+
+    return create_version
+
+
+def create_domain(request, service_id, service_version):
+    """
+    Returns POST create_domain with response json.
+
+    :return: a JSON-serializable dictionary matching the format of the JSON
+             response for fastly_client.create_domain()
+             ("/service/<service_id>/version/<service_version>/domain")
+             request.
+    """
+    request_body = request.args.items()
+    request_dict = dict((k, v) for k, v in request_body)
+    domain_name = request_dict['name'][0]
+    create_domain = {
+        'comment': '',
+        'service_id': service_id,
+        'version': service_version,
+        'name': domain_name}
+
+    global fastly_cache
+    if 'domain_list' not in fastly_cache[service_id]:
+        fastly_cache[service_id]['domain_list'] = []
+
+    fastly_cache[service_id]['domain_list'].append([create_domain,
+                                                   'None', 'False'])
+    return create_domain
+
+
+def check_domains(request, service_id, service_version):
+    """
+    Returns GET check_domains with response json.
+
+    :return: a JSON-serializable dictionary matching the format of the JSON
+             response for fastly_client.check_domain()
+             ("/service/%s/version/%d/domain/check_all")
+             request.
+    """
+    global fastly_cache
+    domain_list = fastly_cache[service_id]['domain_list']
+
+    return domain_list
+
--- mimic/rest/fastly_api.py
+++ mimic/rest/fastly_api.py
@@ -8,7 +8,12 @@ import json
 from twisted.web.server import Request
 
 from mimic.rest.mimicapp import MimicApp
-from mimic.canned_responses.fastly import get_current_customer
+from mimic.canned_responses.fastly import (get_current_customer,
+                                           create_service,
+                                           create_version,
+                                           get_service_by_name,
+                                           create_domain,
+                                           check_domains)
 
 Request.defaultContentType = 'application/json'
 
@@ -26,11 +31,62 @@ class FastlyApi(object):
             authenticating.
         """
         self.core = core
+        self.services = {}
 
     @app.route('/current_customer', methods=['GET'])
     def get_current_customer(self, request):
         """
-        Returns response with random usernames.
+        Returns response with current customer details.
         """
         response = get_current_customer(request)
+        return json.dumps(response)
+
+    @app.route('/service', methods=['POST'])
+    def create_service(self, request):
+        """
+        Returns POST Service.
+        """
+        url_data = request.args.items()
+        response = create_service(request, url_data)
+        return json.dumps(response)
+
+    @app.route('/service/<string:service_id>/version', methods=['POST'])
+    def create_version(self, request, service_id):
+        """
+        Returns POST Service.
+        """
+        response = create_version(request, service_id)
+        return json.dumps(response)
+
+    @app.route('/service/search', methods=['GET'])
+    def get_service_by_name(self, request):
+        """
+        Returns response with current customer details.
+        """
+        url_data = request.args.items()
+        data = dict((key, value) for key, value in url_data)
+        service_name = data['name'][0]
+
+        response = get_service_by_name(request, service_name)
+        return json.dumps(response)
+
+    @app.route(
+        '/service/<string:service_id>/version/<string:version_id>/domain',
+        methods=['POST'])
+    def create_domain(self, request, service_id, version_id):
+        """
+        Returns Create Domain Response.
+        """
+        response = create_domain(request, service_id, version_id)
+        return json.dumps(response)
+
+    @app.route(
+        '/service/<string:service_id>/version/<string:version_id>/domain/'
+        'check_all',
+        methods=['GET'])
+    def check_domains(self, request, service_id, version_id):
+        """
+        Returns Check Domain.
+        """
+        response = check_domains(request, service_id, version_id)
         return json.dumps(response)
