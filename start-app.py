f9b7c15e40ebbecceffd02d0bdd4a95ba61428e3
derwolfe <chriswwolfe@gmail.com>
2014-11-19 07:30:25 -0600

    try starting without using twistd
    This seems to work, but it doesn't seem like the right way to do it. Ideally you should reuse the twistd functionality. This way the application can be daemonized.

--- start-app.py
+++ start-app.py
@@ -4,12 +4,38 @@ start_app.py starts the application for py2app.
 This calls twistd with default arguments.
 """
 
-from mimic.tap import Options, makeService
-from sys import argv
-from twisted.scripts.twistd import run
 
-argv[1:] = [
-    'mimic'
-]
+from mimic.core import MimicCore
+from mimic.resource import MimicRoot
+from twisted.internet.endpoints import (
+    serverFromString,
+    TCP4ServerEndpoint
+)
+from twisted.internet.task import Clock
+from twisted.web.server import Site
 
-run()
+from twisted.python import log
+
+from sys import stdout
+
+
+def startMimic():
+    log.startLogging(stdout)
+
+    from twisted.internet import reactor
+    clock = reactor
+    core = MimicCore.fromPlugins(clock)
+    root = MimicRoot(core, clock)
+    site = Site(root.app.resource())
+    site.displayTracebacks = False
+
+    endpoint = serverFromString(
+        clock,
+        b"tcp:8800:interface=127.0.0.1"
+    )
+    endpoint.listen(site)
+    reactor.run()
+
+
+if __name__ == '__main__':
+    startMimic()
