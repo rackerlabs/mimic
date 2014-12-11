3bb4c5a0ee67e1bf5373f0969b558d58f4b5a30c
derwolfe <chriswwolfe@gmail.com>
2014-12-10 21:22:25 -0600

    remove unused information

--- setup-mac.py
+++ setup-mac.py
@@ -8,19 +8,6 @@ setup(
     name='mimic',
     version='1.3.0',
     description='An API-compatible mock service',
-    packages=find_packages(exclude=[]) + ["twisted.plugins"],
-    package_dir={'mimic': 'mimic'},
-    install_requires=[
-        "characteristic==14.1.0",
-        "klein==0.2.1",
-        "twisted>=13.2.0",
-        "jsonschema==2.0",
-        "treq",
-        "six",
-    ],
-    include_package_data=True,
-    license="Apache License, Version 2.0",
-    # py2app
     app=['start-app.py'],
     options={
         'py2app': {
