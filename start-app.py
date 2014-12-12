385e9b8da53fcd57fce3e32b65cda69f3e7e7d6b
derwolfe <chriswwolfe@gmail.com>
2014-12-11 20:54:04 -0600

    working on connecting them up

---
+++ start-app.py
@@ -0,0 +1,72 @@
+"""
+start_app.py starts the application for py2app.
+"""
+
+from Foundation import *
+from AppKit import *
+
+# import Nib loading functionality from AppKit
+from PyObjCTools import NibClassBuilder, AppHelper
+
+from twisted.internet import _threadedselect
+_threadedselect.install()
+
+from mimic.core import MimicCore
+from mimic.resource import MimicRoot
+from twisted.internet.endpoints import (
+    serverFromString,
+    TCP4ServerEndpoint
+)
+from twisted.internet.task import Clock
+from twisted.web.server import Site
+from twisted.python import log
+
+from sys import stdout
+
+
+class MyAppDelegate(NibClassBuilder.AutoBaseClass):
+    """
+
+    """
+    def applicationDidFinishLaunching_(self, aNotification):
+        """
+        Invoked by NSApplication once the app is done launching and
+        immediately before the first pass through the main event
+        loop.
+        """
+        self.messageTextField.setStringValue_("http://www.twistedmatrix.com/")
+        reactor.interleave(AppHelper.callAfter)
+
+    def applicationShouldTerminate_(self, sender):
+        """
+        Kill the reactor to close cleanly.
+        """
+        if reactor.running:
+            reactor.addSystemEventTrigger(
+                'after', 'shutdown', AppHelper.stopEventLoop)
+            reactor.stop()
+            return False
+        return True
+
+
+def startMimic(reactor):
+    """
+    start mimic
+    """
+    clock = Clock()
+    core = MimicCore.fromPlugins(clock)
+    root = MimicRoot(core, clock)
+    site = Site(root.app.resource())
+    site.displayTracebacks = False
+
+    endpoint = serverFromString(
+        reactor,
+        b"tcp:8800:interface=127.0.0.1"
+    )
+    endpoint.listen(site)
+
+
+if __name__ == '__main__':
+    log.startLogging(stdout)
+    startMimic(reactor)
+    AppHelper.runEventLoop()
