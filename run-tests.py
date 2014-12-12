7c70ba30260cfe1d07c7be17d0c3a6b548ac5376
derwolfe <chriswwolfe@gmail.com>
2014-12-11 18:53:44 -0600

    cleanup import

--- run-tests.py
+++ run-tests.py
@@ -10,17 +10,21 @@ from twisted.trial.runner import (
 )
 from twisted.trial.reporter import VerboseTextReporter
 
+# in order to find the app-bundle, the site-packages.zip
+# file needs to be added to path.
+sys.path.insert(0, b'./lib/python/site-packages.zip')
+
+from mimic import test
+
 
 def runTests():
     """
     run tests from the mimic module.
     """
-    sys.path.insert(0, b'./lib/python/site-packages.zip')
-    from mimic import test
-
     loader = TestLoader()
     suite = loader.loadAnything(test)
     runner = TrialRunner(VerboseTextReporter).run(suite)
+
 
 if __name__ == '__main__':
     runTests()
