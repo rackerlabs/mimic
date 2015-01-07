4ec7b02a360a9dc178c2bcf51a87d70e24b40f07
derwolfe <chriswwolfe@gmail.com>
2015-01-07 17:37:10 -0600

    use more pyenv

--- .travis/install.sh
+++ .travis/install.sh
@@ -20,11 +20,13 @@ if [[ "$DARWIN" = true ]]; then
         py26)
             curl -O https://bootstrap.pypa.io/get-pip.py
             sudo python get-pip.py
+	    pyenv install 2.6
 	    pyenv global 2.6
             ;;
         py27)
             curl -O https://bootstrap.pypa.io/get-pip.py
             sudo python get-pip.py
+	    pyenv install 2.7.8
 	    pyenv global 2.7
             ;;
         pypy)
@@ -39,7 +41,8 @@ if [[ "$DARWIN" = true ]]; then
 	bundle)
 	    curl -O https://bootstrap.pypa.io/get-pip.py
             sudo python get-pip.py
-	    brew install python
+	    pyenv install 2.7.8
+	    pyenv global 2.7.8
 	    ;;
     esac
     pyenv rehash
