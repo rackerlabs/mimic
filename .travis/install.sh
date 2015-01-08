e23607499209f890dadf2283e449013d56f020ea
derwolfe <chriswwolfe@gmail.com>
2015-01-07 18:37:45 -0600

    last build timed out, possibly because of output not showing. That or the hard time cutoff was hit. Is there a faster way to install pyobjc?

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,34 +13,34 @@ if [[ "$DARWIN" = true ]]; then
     brew update
 
     if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
+	eval "$(pyenv init -)"
     fi
 
     case "${TOXENV}" in
-        py26)
-            curl -O https://bootstrap.pypa.io/get-pip.py
-            sudo python get-pip.py
+	py26)
+	    curl -O https://bootstrap.pypa.io/get-pip.py
+	    sudo python get-pip.py
 	    pyenv install 2.6
 	    pyenv global 2.6
-            ;;
-        py27)
-            curl -O https://bootstrap.pypa.io/get-pip.py
-            sudo python get-pip.py
+	    ;;
+	py27)
+	    curl -O https://bootstrap.pypa.io/get-pip.py
+	    sudo python get-pip.py
 	    pyenv install 2.7.8
 	    pyenv global 2.7
-            ;;
-        pypy)
-            brew upgrade pyenv
-            pyenv install pypy-2.4.0
-            pyenv global pypy-2.4.0
-            ;;
-        docs)
-            curl -O https://bootstrap.pypa.io/get-pip.py
-            sudo python get-pip.py
-            ;;
+	    ;;
+	pypy)
+	    brew upgrade pyenv
+	    pyenv install pypy-2.4.0
+	    pyenv global pypy-2.4.0
+	    ;;
+	docs)
+	    curl -O https://bootstrap.pypa.io/get-pip.py
+	    sudo python get-pip.py
+	    ;;
 	bundle)
 	    curl -O https://bootstrap.pypa.io/get-pip.py
-            sudo python get-pip.py
+	    sudo python get-pip.py
 	    pyenv install 2.7.8
 	    pyenv global 2.7.8
 	    ;;
@@ -48,27 +48,31 @@ if [[ "$DARWIN" = true ]]; then
     pyenv rehash
 
 else
+    sudo apt-get -y update
     sudo add-apt-repository -y ppa:fkrull/deadsnakes
 
-    if [[ "${TOXENV}" == "pypy" ]]; then
-        sudo add-apt-repository -y ppa:pypy/ppa
-    fi
-
-    sudo apt-get -y update
-
     case "${TOXENV}" in
-        py26)
-            sudo apt-get install python2.6 python2.6-dev
-            ;;
-        py27)
-            sudo apt-get install python2.7 python2.7-dev
-            ;;
-        pypy)
-            sudo apt-get install --force-yes pypy pypy-dev
-            ;;
-        docs)
-            sudo apt-get install libenchant-dev
-            ;;
+	py26)
+	    sudo apt-get install python2.6 python2.6-dev
+	    ;;
+	py27)
+	    sudo apt-get install python2.7 python2.7-dev
+	    ;;
+	pypy)
+	    sudo add-apt-repository -y ppa:pypy/ppa
+	    sudo apt-get -y update
+	    sudo apt-get install -y pypy pypy-dev
+
+	    # This is required because we need to get rid of the Travis installed PyPy
+	    # or it'll take precedence over the PPA installed one.
+	    sudo rm -rf /usr/local/pypy/bin
+	    ;;
+	docs)
+	    sudo apt-get install libenchant-dev
+	    ;;
+	docs-spellcheck)
+	    sudo apt-get install libenchant-dev
+	    ;;
     esac
 fi
 
--- .travis/run.sh
+++ .travis/run.sh
@@ -3,10 +3,21 @@
 set -e
 set -x
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-    eval "$(pyenv init -)"
-fi
-
 source ~/.venv/bin/activate
-# run the tests for a single env
-tox -e $TOX_ENV
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+
+    # run tox for all but bundle.
+    if [[ "${TOX_ENV}" == 'bundle' ]]; then
+	pip install -r requirements.txt
+	pip install -r dev-requirements.txt
+	pip install -r py2app-requirements.txt
+	make
+    else
+	# run the tests for a single env
+	tox -e $TOX_ENV
+    fi
+else
+    # I realize this is ugly.
+    tox -e $TOX_ENV
+fi
