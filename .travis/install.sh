885b7518b94319cb0cdd2f1cd96fb158bf5a66e8
derwolfe <chriswwolfe@gmail.com>
2015-01-08 19:27:01 -0600

    all working except pypy, osx, clang

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,38 +13,37 @@ if [[ "$DARWIN" = true ]]; then
     brew update
 
     if which pyenv > /dev/null; then
-	eval "$(pyenv init -)"
+        eval "$(pyenv init -)"
     fi
 
     case "${TOX_ENV}" in
-	py26)
-	    curl -O https://bootstrap.pypa.io/get-pip.py
-	    sudo python get-pip.py
-	    pyenv install 2.6.9
-	    pyenv global 2.6.9
-	    python --version
-	    ;;
-	py27)
-	    curl -O https://bootstrap.pypa.io/get-pip.py
-	    sudo python get-pip.py
-	    pyenv install 2.7.8
-	    pyenv global 2.7.8
-	    ;;
-	pypy)
-	    brew upgrade pyenv
-	    pyenv install pypy-2.4.0
-	    pyenv global pypy-2.4.0
-	    ;;
-	docs)
-	    curl -O https://bootstrap.pypa.io/get-pip.py
-	    sudo python get-pip.py
-	    ;;
-	bundle)
-	    curl -O https://bootstrap.pypa.io/get-pip.py
-	    sudo python get-pip.py
-	    pyenv install 2.7.8
-	    pyenv global 2.7.8
-	    ;;
+        py26)
+            curl -O https://bootstrap.pypa.io/get-pip.py
+            sudo python get-pip.py
+            pyenv install 2.6.9
+            pyenv global 2.6.9
+            ;;
+        py27)
+            curl -O https://bootstrap.pypa.io/get-pip.py
+            sudo python get-pip.py
+            pyenv install 2.7.8
+            pyenv global 2.7.8
+            ;;
+        pypy)
+            brew upgrade pyenv
+            pyenv install pypy-2.4.0
+            pyenv global pypy-2.4.0
+            ;;
+        docs)
+            curl -O https://bootstrap.pypa.io/get-pip.py
+            sudo python get-pip.py
+            ;;
+        bundle)
+            curl -O https://bootstrap.pypa.io/get-pip.py
+            sudo python get-pip.py
+            pyenv install 2.7.8
+            pyenv global 2.7.8
+            ;;
     esac
     pyenv rehash
 
@@ -59,21 +58,21 @@ else
     sudo apt-get -y update
 
     case "${TOX_ENV}" in
-	py26)
-	    sudo apt-get install python2.6 python2.6-dev
-	    ;;
-	py27)
-	    sudo apt-get install python2.7 python2.7-dev
-	    ;;
-	pypy)
-	    sudo apt-get install --force-yes pypy pypy-dev
-	    ;;
-	docs)
-	    sudo apt-get install libenchant-dev
-	    ;;
-	docs-spellcheck)
-	    sudo apt-get install libenchant-dev
-	    ;;
+        py26)
+            sudo apt-get install python2.6 python2.6-dev
+            ;;
+        py27)
+            sudo apt-get install python2.7 python2.7-dev
+            ;;
+        pypy)
+            sudo apt-get install --force-yes pypy pypy-dev
+            ;;
+        docs)
+            sudo apt-get install libenchant-dev
+            ;;
+        docs-spellcheck)
+            sudo apt-get install libenchant-dev
+            ;;
     esac
 fi
 
