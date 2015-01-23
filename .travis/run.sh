ef3887c1177a3ac20d6d9666ca778220a7a95de3
derwolfe <chriswwolfe@gmail.com>
2015-01-22 20:24:05 -0600

    opps - specify the location of the venv

--- .travis/run.sh
+++ .travis/run.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 if [[ "${TOX_ENV}" == "bundle" ]]; then
-    virtualenv -p /usr/bin/python2.7 venv2
+    virtualenv -p /usr/bin/python2.7 ~/.venv2
     source ~/.venv2/bin/activate
     pip install -r requirements.txt
     pip install -r dev-requirements.txt
