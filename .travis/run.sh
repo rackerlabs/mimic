120ba3426ac01a5d56f8736d238ed27180081c7c
derwolfe <chriswwolfe@gmail.com>
2015-01-22 22:03:14 -0600

    see what builder shows - ttrying to replicate tests run in local environment

--- .travis/run.sh
+++ .travis/run.sh
@@ -9,8 +9,7 @@ if [[ "${TOX_ENV}" == "bundle" ]]; then
     pip install -r requirements.txt
     pip install -r dev-requirements.txt
     pip install -r py2app-requirements.txt
-    make build
-    make test
+    make
 else
     source ~/.venv/bin/activate
     tox -e $TOX_ENV -- $TOX_FLAGS
