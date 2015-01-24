2fb2a0d5ae49bea2788fedab71929352a7d08516
derwolfe <chriswwolfe@gmail.com>
2015-01-24 14:53:47 -0600

    no notification

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,15 +3,5 @@
 set -e
 set -x
 
-if [[ "${TOX_ENV}" == "bundle" ]]; then
-    virtualenv ~/.venv2 -p /usr/local/bin/python2.7
-    source ~/.venv2/bin/activate
-    pip install -r requirements.txt
-    pip install -r dev-requirements.txt
-    pip install -r py2app-requirements.txt
-    make build
-    make test
-else
-    source ~/.venv/bin/activate
-    tox -e $TOX_ENV -- $TOX_FLAGS
-fi
+source ~/.venv/bin/activate
+tox -e $TOX_ENV -- $TOX_FLAGS
