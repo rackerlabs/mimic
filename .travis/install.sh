8b87a2215688acd38d2e166ef29dd726ba17c9d4
derwolfe <chriswwolfe@gmail.com>
2015-01-24 14:36:51 -0600

    Revert "No need to install brew python"
    This reverts commit ce903bcffdccf4c9c819fd391c783dcc90e90f37.

--- .travis/install.sh
+++ .travis/install.sh
@@ -29,7 +29,7 @@ if [[ "$DARWIN" = true ]]; then
             pyenv global 2.7.8
             ;;
         pypy)
-            # travis/tox are not finding pypy when installed using pyenv.
+	    # travis/tox are not finding pypy when installed using pyenv.
             brew install pypy
             ;;
         docs)
@@ -37,8 +37,7 @@ if [[ "$DARWIN" = true ]]; then
             sudo python get-pip.py
             ;;
         bundle)
-            curl -O https://bootstrap.pypa.io/get-pip.py
-            sudo python get-pip.py
+            brew install python
             ;;
     esac
     pyenv rehash
