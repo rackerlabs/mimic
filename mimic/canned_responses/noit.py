f424c53faa2f5907e27c0b4eff9fd09949f2160f
lekhajee <lekha.jeevan@rackspace.com>
2015-01-26 18:30:55 -0800

    included mocks for test check and delete check. fixtures for metrics

--- mimic/canned_responses/noit.py
+++ mimic/canned_responses/noit.py
@@ -1,6 +1,8 @@
 """
 Canned response for Nova
 """
+from mimic.canned_responses.noit_metrics_fixture import (metrics_common_template,
+                                                          metrics)
 
 noit_cache = {}
 
@@ -41,6 +43,15 @@ def create_check(request, check_id):
     return
 
 
+def test_check(check_type):
+    """
+
+    """
+    if metrics_common_template["check"]["state"].get("metric"):
+        del metrics_common_template["check"]["state"]["metric"]
+    metrics_common_template["check"]["state"].update(metrics.get(check_type, {}))
+    return metrics_common_template
+
 def get_check(check_id):
     """
     Gets the check_id from noit_cache
@@ -48,8 +59,19 @@ def get_check(check_id):
     return noit_cache[check_id]
 
 
-def get_checks(check_id):
+def get_checks():
     """
     Gets all checks from noit_cache
     """
     return noit_cache
+
+
+def delete_check(check_id):
+    """
+    Delete the check_id from noit cache
+    """
+    try:
+        del noit_cache[check_id]
+    except:
+        return 404
+    return
--- mimic/rest/noit_api.py
+++ mimic/rest/noit_api.py
@@ -3,9 +3,20 @@
 Defines get token, impersonation
 """
 import xmltodict
+import json
 from twisted.web.server import Request
 from mimic.rest.mimicapp import MimicApp
-from mimic.canned_responses.noit import create_check, get_check, get_checks
+from mimic.canned_responses.noit import (create_check, get_check, get_checks,
+                                         delete_check, test_check)
+
+
+### TO DO:
+# Convert responses to XML
+# Include all check type to the metrics in the fixtures
+# Fixture to populate as per test check request
+# Move away from using dict for the templates
+# add tests
+# Include errors such as 500s on create check
 
 
 Request.defaultContentType = 'application/xml'
@@ -26,8 +37,35 @@ class NoitApi(object):
         """
         self.core = core
 
+    def validate_check_payload(self, request):
+        """
+        Validate the check request payload and returns the response code
+        """
+        content = str(request.content.read())
+        payload = xmltodict.parse(content)
+        attributes = ["name", "module", "target", "period", "timeout",
+                      "filterset"]
+        for each in attributes:
+            if not payload["check"]["attributes"].get(each):
+                return 404, None
+        return 200, payload["check"]["attributes"]
+
+
+    @app.route('/checks/test', methods=['POST'])
+    def test_check(self, request):
+        """
+        Validates the check xml and returns ????
+        """
+        response = self.validate_check_payload(request)
+        if (response[0] == 200):
+            # request.setHeader("content-type", "application/xml")
+            response_body = test_check(response[1]["module"])
+            return json.dumps(response_body)
+        request.setResponseCode(response[0])
+        return
+
     @app.route('/checks/set/<check_id>', methods=['PUT'])
-    def set_check(self, check_id, request):
+    def set_check(self, request, check_id):
         """
         Creates a check for the given check_id. If the check_id already exists, then
         it updates that check.
@@ -35,31 +73,37 @@ class NoitApi(object):
             to see the response code expected)
         """
         # validate check_id is a uuid ?? does noit fail if not?
-        content = xmltodict(request)
-        attributes = ["name", "module", "target", "period", "timeout",
-                      "filterset"]
-        request.setResponseCode(200)
-        request.setHeader("content-type", "application/xml")
-        for each in attributes:
-            if not content.get("each"):
-                request.setResponseCode(400)
-                return
-        return create_check(content, check_id)
+
+        response = self.validate_check_payload(request)
+        request.setResponseCode(response[0])
+        if (response[0] == 200):
+            # construct xml response
+            return create_check(response[1], check_id)
+        return
 
     @app.route('/check/show/<check_id>', methods=['GET'])
     def get_checks(self, request, check_id):
         """
         Return the current configuration and state of the specified check.
         """
-        request.setResponseCode(200)
-        request.setHeader("content-type", "application/xml")
-        return get_check(check_id)
+        # request.setHeader("content-type", "application/xml")
+        return json.dumps(get_check(check_id))
 
     @app.route('/checks', methods=['GET'])
     def get_all_checks(self, request):
         """
-        Return the current configuration and state of the specified check.
+        Return the current configuration and state of all checks.
         """
-        request.setResponseCode(200)
+        # request.setHeader("content-type", "application/xml")
+        return json.dumps(get_checks())
+
+
+    @app.route('/checks/delete/<check_id>', methods=['DELETE'])
+    def delete_checks(self, request, check_id):
+        """
+        Delete the specified check and return 204 response code
+        """
+        response_code = delete_check(check_id) or  200
+        request.setResponseCode(response_code)
         request.setHeader("content-type", "application/xml")
-        return get_checks()
+        return
