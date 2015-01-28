2f11995012c5b38475a8862babe714968ccfbd60
lekhajee <lekha.jeevan@rackspace.com>
2015-01-27 23:22:27 -0800

    use request_with_content from the test helper instead of request

--- mimic/test/test_noit.py
+++ mimic/test/test_noit.py
@@ -1,12 +1,12 @@
 import uuid
 import xmltodict
-
+import json
 from twisted.trial.unittest import SynchronousTestCase
 from twisted.internet.task import Clock
 
 from mimic.core import MimicCore
 from mimic.resource import MimicRoot
-from mimic.test.helpers import request
+from mimic.test.helpers import request_with_content
 
 
 class NoitAPITests(SynchronousTestCase):
@@ -37,27 +37,27 @@ class NoitAPITests(SynchronousTestCase):
         self.check_id = uuid.uuid4()
         url = "noit/checks/set/{0}".format(self.check_id)
 
-        req = request(self, self.root, "PUT", url,
-                      body=self.create_check_xml_payload,
-                      headers={'content-type': ['application/xml']})
-        self.response = self.successResultOf(req)
+        (self.response, self.body) = self.successResultOf(
+            request_with_content(self, self.root, "PUT", url,
+                                 body=self.create_check_xml_payload))
 
     def test_get_all_checks(self):
         """
         Test to verify :func:`get_all_checks` on ``GET /config/checks``
         """
-        req = request(self, self.root, "GET", "noit/config/checks")
-        response = self.successResultOf(req)
+        (response, body) = self.successResultOf(
+            request_with_content(self, self.root, "GET", "noit/config/checks"))
         self.assertEqual(response.code, 200)
+        json_response = json.loads(json.dumps(xmltodict.parse(body)))
+        self.assertGreater(len(json_response["checks"]["check"]), 0)
 
     def test_test_check(self):
         """
         Test to verify :func:`test_check` on ``POST /checks/test``
         """
-        req = request(self, self.root, "POST", "noit/checks/test",
-                      body=self.create_check_xml_payload,
-                      headers={'content-type': ['application/xml']})
-        response = self.successResultOf(req)
+        (response, body) = self.successResultOf(
+            request_with_content(self, self.root, "POST", "noit/checks/test",
+                                 body=self.create_check_xml_payload))
         self.assertEqual(response.code, 200)
 
     def test_create_check(self):
@@ -72,21 +72,20 @@ class NoitAPITests(SynchronousTestCase):
         ``PUT /checks/set/<check_id>``
         """
         self.create_check["check"]["attributes"]["names"] = "rename"
-        req = request(self, self.root, "PUT",
-                      "noit/checks/set/{0}".format(self.check_id),
-                      body=xmltodict.unparse(self.create_check
-                                             ).encode("utf-8"),
-                      headers={'content-type': ['application/xml']})
-        self.response = self.successResultOf(req)
+        (response, body) = self.successResultOf(
+            request_with_content(self, self.root, "PUT",
+                                 "noit/checks/set/{0}".format(self.check_id),
+                                 body=xmltodict.unparse(self.create_check
+                                                        ).encode("utf-8")))
         self.assertEqual(self.response.code, 200)
 
     def test_get_check(self):
         """
         Test to verify :func:`get_checks` on ``GET /checks/show/<check_id>``
         """
-        get_req = request(self, self.root, "GET",
-                          "noit/checks/show/{0}".format(self.check_id))
-        get_response = self.successResultOf(get_req)
+        (get_response, body) = self.successResultOf(
+            request_with_content(self, self.root, "GET",
+                                 "noit/checks/show/{0}".format(self.check_id)))
         self.assertEqual(get_response.code, 200)
 
     def test_delete_check(self):
@@ -94,9 +93,9 @@ class NoitAPITests(SynchronousTestCase):
         Test to verify :func:`delete_checks` on
         ``DELETE /checks/delete/<check_id>``
         """
-        del_req = request(self, self.root, "DELETE",
-                          "noit/checks/delete/{0}".format(self.check_id))
-        del_response = self.successResultOf(del_req)
+        (del_response, body) = self.successResultOf(
+            request_with_content(self, self.root, "DELETE",
+                                 "noit/checks/delete/{0}".format(self.check_id)))
         self.assertEqual(del_response.code, 200)
 
     def test_create_check_fails_with_500(self):
@@ -104,12 +103,11 @@ class NoitAPITests(SynchronousTestCase):
         Test to verify :func:`set_check` results in error 500,
         when the xml cannot be parsed.
         """
-        req = request(self, self.root, "PUT",
-                      "noit/checks/set/{0}".format(self.check_id),
-                      body=self.create_check_xml_payload.replace(
-                          '</check>', ' abc'),
-                      headers={'content-type': ['application/xml']})
-        response = self.successResultOf(req)
+        (response, body) = self.successResultOf(
+            request_with_content(self, self.root, "PUT",
+                                 "noit/checks/set/{0}".format(self.check_id),
+                                 body=self.create_check_xml_payload.replace(
+                                     '</check>', ' abc')))
         self.assertEqual(response.code, 500)
 
     def test_create_check_fails_with_500_for_invalid_check_id(self):
@@ -117,9 +115,8 @@ class NoitAPITests(SynchronousTestCase):
         Test to verify :func:`set_check` results in error 500,
         when the xml cannot be parsed.
         """
-        req = request(self, self.root, "PUT",
-                      "noit/checks/set/123444",
-                      body=self.create_check_xml_payload,
-                      headers={'content-type': ['application/xml']})
-        response = self.successResultOf(req)
+        (response, body) = self.successResultOf(
+            request_with_content(self, self.root, "PUT",
+                                 "noit/checks/set/123444",
+                                 body=self.create_check_xml_payload))
         self.assertEqual(response.code, 500)
