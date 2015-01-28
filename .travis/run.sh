93eae94bdc1776895f67f5d6026e08ae3fbbfd9f
derwolfe <chriswwolfe@gmail.com>
2015-01-27 20:46:01 -0600

    Travis builds standalone and system python application

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,5 +3,26 @@
 set -e
 set -x
 
-source ~/.venv/bin/activate
-tox -e $TOX_ENV -- $TOX_FLAGS
+
+case "${TOX_ENV}" in
+    bundle)
+	virtualenv ~/.venv2 -p /usr/local/bin/python2.7
+	source ~/.venv2/bin/activate
+	pip install -r requirements.txt
+	pip install -r dev-requirements.txt
+	pip install -r py2app-requirements.txt
+	make build
+	make test
+    ;;
+    bundle-system)
+	virtualenv ~/.venv2 -p /usr/bin/python2.7 --system-site-packages
+	source ~/.venv2/bin/activate
+	pip install -r requirements.txt
+	pip install -r dev-requirements.txt
+	make build
+	make test
+    ;;
+    *)
+	source ~/.venv/bin/activate
+	tox -e $TOX_ENV
+esac
