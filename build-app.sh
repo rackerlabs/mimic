3a77a9862a7da0569524068c7e6c38a2ed62c03c
derwolfe <chriswwolfe@gmail.com>
2015-02-07 09:33:50 -0600

    remove clean

--- build-app.sh
+++ build-app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-find . -name 'dist' -print0 | xargs rm -rf
-find . -name 'build' -print0 | xargs rm -rf
+
+#find . -name 'dist' -print0 | xargs rm -rf
+#find . -name 'build' -print0 | xargs rm -rf
 python setup.py py2app
 ./dist/mimic.app/Contents/MacOS/run-tests
