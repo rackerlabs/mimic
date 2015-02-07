794efab63f9e59cbfca06ac813b54171155fae09
derwolfe <chriswwolfe@gmail.com>
2015-02-07 10:08:00 -0600

    comments

--- build-app.sh
+++ build-app.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-#find . -name 'dist' -print0 | xargs rm -rf
-#find . -name 'build' -print0 | xargs rm -rf
+# clean out any previous artificats from py2app builds
+find . -name 'dist' -print0 | xargs rm -rf
+find . -name 'build' -print0 | xargs rm -rf
+
+# build the application
 python setup.py py2app
+
+# run the the application's tests
 ./dist/mimic.app/Contents/MacOS/run-tests
