311c24ce9154b16e2e029d05a778f14ca2b2841b
derwolfe <chriswwolfe@gmail.com>
2015-02-08 13:12:19 -0600

    make comments more specific

--- build-app.sh
+++ build-app.sh
@@ -4,6 +4,8 @@
 find . -name 'dist' -print0 | xargs rm -rf
 find . -name 'build' -print0 | xargs rm -rf
 
+# build a virtualenv which will be used for all of the application's
+# dependencies if one doesn't already exist
 if [[ ! -d ./venv-app ]]; then
     virtualenv ./venv-app -p /usr/bin/python2.7 --system-site-packages
 fi
@@ -13,8 +15,7 @@ source  ./venv-app/bin/activate
 # install the dependencies for the main application
 pip install -r requirements.txt
 
-# install minimum needed dependencies or confirm
-# that they are installed
+# install dependencies that are needed to build and run the mac application
 pip install -r py2app-requirements.txt
 
 # build the application using py2app
