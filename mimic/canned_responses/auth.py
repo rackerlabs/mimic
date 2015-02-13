7c140e66d9e3f101f8cf88a7e34d13f220f0c9df
lekhajee <lekha.jeevan@rackspace.com>
2015-02-12 19:49:48 -0800

    include impersonator roles

--- mimic/canned_responses/auth.py
+++ mimic/canned_responses/auth.py
@@ -11,7 +11,8 @@ GLOBAL_MUTABLE_TOKEN_STORE = {}
 HARD_CODED_TOKEN = "fff73937db5047b8b12fc9691ea5b9e8"
 HARD_CODED_USER_ID = "10002"
 HARD_CODED_USER_NAME = "mimictestuser"
-HARD_CODED_ROLES = [{"id": "3", "description": "User Admin Role.",
+HARD_CODED_ROLES = [{"id": "3",
+                     "description": "User Admin Role.",
                      "name": "identity:user-admin"}]
 
 
@@ -22,6 +23,38 @@ def format_timestamp(dt):
     :param datetime.datetime dt: A datetime.datetime object to be formatted.
     """
     return dt.strftime('%Y-%m-%dT%H:%M:%S.999-05:00')
+
+
+def impersonator_user_role(id, name):
+    """
+    Canned response for validate token if the token being validated
+    is an impersonated token.
+    """
+    return {
+        "id": id,
+        "roles": [
+            {"id": "10000001",
+             "serviceId": "test-eabb70a0e702a4626977c331d5c4",
+             "description": "Service admin role for cloud feeds access. Assign only to service users",
+             "name": "cloudfeeds:service-admin"},
+            {"id": "10000002",
+             "serviceId": "test-eabb70a0e702a4626977c331d5c4",
+             "description": "Checkmate Access role",
+             "name": "checkmate"},
+            {"id": "100000003",
+             "serviceId": "test-eabb70a0e702a4626977c331d5c4",
+             "description": "Service admin role for Monitoring access. Assign only to service users",
+             "name": "monitoring:service-admin"},
+            {"id": "100000004",
+             "serviceId": "test-eabb70a0e702a4626977c331d5c4",
+             "description": "Admin Role.",
+             "name": "identity:admin"},
+            {"id": "10000005",
+             "serviceId": "test-d4614b87411e141fe8109099bc4f",
+             "description": "Role to access Customer service as an Admin",
+             "name": "customer:admin"}],
+        "name": name
+    }
 
 
 def get_token(tenant_id,
