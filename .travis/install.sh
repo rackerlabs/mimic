1e64e06ab013d3f6b902979361179a846817ab2a
Glyph <glyph@twistedmatrix.com>
2015-02-15 18:37:28 -0800

    one potato

--- .travis/install.sh
+++ .travis/install.sh
@@ -80,3 +80,4 @@ tox -e "${TOX_ENV}" --recreate --notest
 # "set -e", the whole script, so travis will retry it) if we didn't get to the
 # end stage of "inst" (i.e. installing mimic itself).
 ./.tox/"${TOX_ENV}"/bin/pip freeze | grep -e '^mimic=='
+
