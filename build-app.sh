06c2e9fe8450fda4cbe393a072f3f0ac89b86579
Glyph <glyph@twistedmatrix.com>
2015-03-03 22:25:18 -0800

    no more virtualenv for now

--- build-app.sh
+++ build-app.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
+THIS_SCRIPT="$0";
+
+cd "$(dirname ${THIS_SCRIPT})";
+
 # cleanup any old build artifacts
-find . -name 'dist' -print0 | xargs rm -rf
-find . -name 'build' -print0 | xargs rm -rf
+rm -fr ./dist ./build;
 
-# build a virtualenv which will be used for all of the application's
-# dependencies if one doesn't already exist
-if [[ ! -d ./venv-app ]]; then
-    virtualenv ./venv-app -p /usr/bin/python2.7 --system-site-packages
-fi
-
-source  ./venv-app/bin/activate
+# We should really be using a virtualenv for this, but unfortunately due to
+# this bug:
+# https://bitbucket.org/ronaldoussoren/py2app/issue/156/virtualenvpy-recipe-calls-methods-renamed#comment-None
+# virtualenv-based py2app installations are presently failing.  So, for the
+# moment, we require --user installations of everything.
 
 # install the dependencies for the main application
-pip install -r requirements.txt
+pip install --user -r requirements.txt;
 
 # install dependencies that are needed to build and run the mac application
-pip install -r py2app-requirements.txt
+pip install --user -r py2app-requirements.txt;
 
 # build the application using py2app
-python setup.py py2app
+python setup.py py2app;
 
 # run the tests for application that py2app built
-./dist/mimic.app/Contents/MacOS/run-tests
+./dist/mimic.app/Contents/MacOS/run-tests;
