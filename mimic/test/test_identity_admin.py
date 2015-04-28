ae54e72e52d98a8c3bdb0ada15acff38c849d81b
Laurens Van Houtven <_@lvh.io>
2015-04-28 12:27:24 -0700

    Write a validation test

--- mimic/test/test_identity_admin.py
+++ mimic/test/test_identity_admin.py
@@ -1,6 +1,7 @@
 """
 Tests for the identity admin API.
 """
+from jsonschema import validate
 
 from twisted.plugin import IPlugin
 from twisted.trial.unittest import SynchronousTestCase
@@ -9,7 +10,7 @@ from twisted.web.resource import IResource
 from zope.interface.verify import verifyObject
 
 from mimic.imimic import IAPIMock
-from mimic.rest.identity_admin_api import IdentityAdminAPI
+from mimic.rest import identity_admin_api as api
 
 
 class IdentityAdminAPITests(SynchronousTestCase):
@@ -20,7 +21,7 @@ class IdentityAdminAPITests(SynchronousTestCase):
         """
         Create a identity API mock instance for testing.
         """
-        self.mock = IdentityAdminAPI()
+        self.mock = api.IdentityAdminAPI()
 
     def test_interface(self):
         """
@@ -37,3 +38,28 @@ class IdentityAdminAPITests(SynchronousTestCase):
         store = None
         resource = self.mock.resource_for_region("REG", "prefix", store)
         verifyObject(IResource, resource)
+
+
+class CreateEndpointTemplateSchemaTests(SynchronousTestCase):
+    """
+    Tests for the schema for endpoint template creation.
+    """
+    def test_validates(self):
+        """
+        Test simple schema validation.
+        """
+        body = {
+            "OS-KSCATALOG:endpointTemplate": {
+                "region": "North",
+                "name": "Compute",
+                "type": "compute",
+                "publicURL": "https://compute.north.public.com/v1",
+                "internalURL": "https://compute.north.internal.com/v1",
+                "adminURL": "https://service-admin.com/v1",
+                "versionId": "1",
+                "versionInfo": "https://compute.north.public.com/v1/",
+                "versionList": "https://compute.north.public.com/",
+                "RAX-AUTH:tenantAlias": "{tenant}",
+            }
+        }
+        validate(body, api.create_endpoint_template_schema)
