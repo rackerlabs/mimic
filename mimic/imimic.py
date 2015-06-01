f6a6e5294444a9f4d792d54d57223bec5ab263ad
cyli <cyli@twistedmatrix.com>
2015-06-01 16:43:53 -0700

    Provide a decorator that makes generic behavior CRUD API endpoints

--- mimic/imimic.py
+++ mimic/imimic.py
@@ -45,3 +45,12 @@ class ICredential(Interface):
             single tenant for all plugins across all regions.
         :rtype: :class:`mimic.session.SessionStore`
         """
+
+
+class IBehaviorAPI(Interface):
+    """
+    An :obj:`IBehaviorAPI` is an object that has Klein route handlers for
+    standard :mod:`mimic.model.behavior` CRUD.
+    """
+    app = Attribute("A Klein instance.")
+    registry_collection = Attribute("A BehaviorRegistryCollection instance.")
