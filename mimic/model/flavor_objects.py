615becf618c86c9d77376f8b70c21b9e18255220
Tommy Walton <tommy.walton@RACKSPACE.COM>
2015-08-26 18:15:48 -0400

    add Rackspace flavors

--- mimic/model/flavor_objects.py
+++ mimic/model/flavor_objects.py
@@ -66,7 +66,7 @@ class Flavor(object):
             "links": self.links_json(absolutize_url),
             "name": self.name,
             "ram": self.ram,
-            "vcups": self.vcpus,
+            "vcpus": self.vcpus,
             "rxtx_factor": self.rxtx,
             "disk": self.disk,
             "OS-FLV-WITH-EXT-SPECS:extra_specs": self.extra_specs_json()
@@ -78,13 +78,20 @@ class RackspaceStandardFlavor(Flavor):
     """
     A Rackspace standard flavor object representation
     """
-    flavors = {"512 MB Standard Instance": {"ram": 512, "vcpus": 1, "rxtx_factor": 80, "disk": 20},
-               "1 GB Standard Instance": {"ram": 1024, "vcpus": 1, "rxtx_factor": 120, "disk": 40},
-               "2 GB Standard Instance": {"ram": 2048, "vcpus": 2, "rxtx_factor": 240, "disk": 80},
-               "4 GB Standard Instance": {"ram": 4096, "vcpus": 2, "rxtx_factor": 400, "disk": 160},
-               "8 GB Standard Instance": {"ram": 8192, "vcpus": 4, "rxtx_factor": 600, "disk": 320},
-               "15 GB Standard Instance": {"ram": 15360, "vcpus": 6, "rxtx_factor": 800, "disk": 620},
-               "30 GB Standard Instance": {"ram": 30720, "vcpus": 8, "rxtx_factor": 1200, "disk": 1200}}
+    flavors = {"512 MB Standard Instance": {"id": "2", "ram": 512, "vcpus": 1, "rxtx_factor": 80,
+                                            "disk": 20},
+               "1 GB Standard Instance": {"id": "3", "ram": 1024, "vcpus": 1, "rxtx_factor": 120,
+                                          "disk": 40},
+               "2 GB Standard Instance": {"id": "4", "ram": 2048, "vcpus": 2, "rxtx_factor": 240,
+                                          "disk": 80},
+               "4 GB Standard Instance": {"id": "5", "ram": 4096, "vcpus": 2, "rxtx_factor": 400,
+                                          "disk": 160},
+               "8 GB Standard Instance": {"id": "6", "ram": 8192, "vcpus": 4, "rxtx_factor": 600,
+                                          "disk": 320},
+               "15 GB Standard Instance": {"id": "7", "ram": 15360, "vcpus": 6, "rxtx_factor": 800,
+                                           "disk": 620},
+               "30 GB Standard Instance": {"id": "8", "ram": 30720, "vcpus": 8, "rxtx_factor": 1200,
+                                           "disk": 1200}}
 
     def extra_specs_json(self):
         """
@@ -99,13 +106,18 @@ class RackspaceStandardFlavor(Flavor):
 
 class RackspaceComputeFlavor(Flavor):
     """
-    A Rackspace compute flavor object representation
+    A Rackspace compute flavor object representatione
     """
-    flavors = {"3.75 GB Compute v1": {"ram": 3840, "vcpus": 2, "rxtx_factor": 312.5, "disk": 0},
-               "7.5 GB Compute v1": {"ram": 7680, "vcpus": 4, "rxtx_factor": 625, "disk": 0},
-               "15 GB Compute v1": {"ram": 15360, "vcpus": 8, "rxtx_factor": 1250, "disk": 0},
-               "30 GB Compute v1": {"ram": 30720, "vcpus": 16, "rxtx_factor": 2500, "disk": 0},
-               "60 GB Compute v1": {"ram": 61440, "vcpus": 32, "rxtx_factor": 5000, "disk": 0}}
+    flavors = {"3.75 GB Compute v1": {"id": "compute1-8", "ram": 3840, "vcpus": 2, "rxtx_factor": 312.5,
+                                      "disk": 0},
+               "7.5 GB Compute v1": {"id": "compute1-4", "ram": 7680, "vcpus": 4, "rxtx_factor": 625,
+                                     "disk": 0},
+               "15 GB Compute v1": {"id": "compute1-15", "ram": 15360, "vcpus": 8, "rxtx_factor": 1250,
+                                    "disk": 0},
+               "30 GB Compute v1": {"id": "compute1-30", "ram": 30720, "vcpus": 16, "rxtx_factor": 2500,
+                                    "disk": 0},
+               "60 GB Compute v1": {"id": "compute1-60", "ram": 61440, "vcpus": 32, "rxtx_factor": 5000,
+                                    "disk": 0}}
 
     def extra_specs_json(self):
         """
@@ -122,10 +134,14 @@ class RackspaceGeneralFlavor(Flavor):
     """
     A Rackspace general flavor object representation
     """
-    flavors = {"1 GB General Purpose v1": {"ram": 1024, "vcpus": 1, "rxtx_factor": 200, "disk": 20},
-               "2 GB General Purpose v1": {"ram": 2048, "vcpus": 2, "rxtx_factor": 400, "disk": 40},
-               "4 GB General Purpose v1": {"ram": 4096, "vcpus": 4, "rxtx_factor": 800, "disk": 80},
-               "8 GB General Purpose v1": {"ram": 8192, "vcpus": 8, "rxtx_factor": 1600, "disk": 160}}
+    flavors = {"1 GB General Purpose v1": {"id": "general1-1", "ram": 1024, "vcpus": 1,
+                                           "rxtx_factor": 200, "disk": 20},
+               "2 GB General Purpose v1": {"id": "general1-2", "ram": 2048, "vcpus": 2,
+                                           "rxtx_factor": 400, "disk": 40},
+               "4 GB General Purpose v1": {"id": "general1-4", "ram": 4096, "vcpus": 4,
+                                           "rxtx_factor": 800, "disk": 80},
+               "8 GB General Purpose v1": {"id": "general1-8", "ram": 8192, "vcpus": 8,
+                                           "rxtx_factor": 1600, "disk": 160}}
 
     def extra_specs_json(self):
         """
@@ -142,11 +158,16 @@ class RackspaceIOFlavor(Flavor):
     """
     A Rackspace IO flavor object representation
     """
-    flavors = {"15 GB I/0 v1": {"ram": 15360, "vcpus": 4, "rxtx_factor": 1250, "disk": 40},
-               "30 GB I/0 v1": {"ram": 30720, "vcpus": 8, "rxtx_factor": 2500, "disk": 40},
-               "60 GB I/0 v1": {"ram": 61440, "vcpus": 16, "rxtx_factor": 5000, "disk": 40},
-               "90 GB I/0 v1": {"ram": 92160, "vcpus": 24, "rxtx_factor": 7500, "disk": 40},
-               "120 GB I/0 v1": {"ram": 122880, "vcpus": 32, "rxtx_factor": 10000, "disk": 40}}
+    flavors = {"15 GB I/0 v1": {"id": "io1-15", "ram": 15360, "vcpus": 4, "rxtx_factor": 1250,
+                                "disk": 40},
+               "30 GB I/0 v1": {"id": "io1-30", "ram": 30720, "vcpus": 8, "rxtx_factor": 2500,
+                                "disk": 40},
+               "60 GB I/0 v1": {"id": "io1-60", "ram": 61440, "vcpus": 16, "rxtx_factor": 5000,
+                                "disk": 40},
+               "90 GB I/0 v1": {"id": "io1-90", "ram": 92160, "vcpus": 24, "rxtx_factor": 7500,
+                                "disk": 40},
+               "120 GB I/0 v1": {"id": "io1-120", "ram": 122880, "vcpus": 32, "rxtx_factor": 10000,
+                                 "disk": 40}}
 
     def extra_specs_json(self):
         """
@@ -163,11 +184,16 @@ class RackspaceMemoryFlavor(Flavor):
     """
     A Rackspace memory flavor object representation
     """
-    flavors = {"15 GB Memory v1": {"ram": 15360, "vcpus": 2, "rxtx_factor": 625, "disk": 0},
-               "30 GB Memory v1": {"ram": 30720, "vcpus": 4, "rxtx_factor": 1250, "disk": 0},
-               "60 GB Memory v1": {"ram": 61440, "vcpus": 8, "rxtx_factor": 2500, "disk": 0},
-               "120 GB Memory v1": {"ram": 122880, "vcpus": 16, "rxtx_factor": 5000, "disk": 0},
-               "240 GB Memory v1": {"ram": 245760, "vcpus": 32, "rxtx_factor": 10000, "disk": 0}}
+    flavors = {"15 GB Memory v1": {"id": "memory1-15", "ram": 15360, "vcpus": 2, "rxtx_factor": 625,
+                                   "disk": 0},
+               "30 GB Memory v1": {"id": "memory1-30", "ram": 30720, "vcpus": 4, "rxtx_factor": 1250,
+                                   "disk": 0},
+               "60 GB Memory v1": {"id": "memory1-60", "ram": 61440, "vcpus": 8, "rxtx_factor": 2500,
+                                   "disk": 0},
+               "120 GB Memory v1": {"id": "memory1-120", "ram": 122880, "vcpus": 16, "rxtx_factor": 5000,
+                                    "disk": 0},
+               "240 GB Memory v1": {"id": "memory1-240", "ram": 245760, "vcpus": 32,
+                                    "rxtx_factor": 10000, "disk": 0}}
 
     def extra_specs_json(self):
         """
@@ -184,9 +210,12 @@ class RackspaceOnMetalFlavor(Flavor):
     """
     A Rackspace onMetal flavor object representation
     """
-    flavors = {"OnMetal Compute v1": {"ram": 32768, "vcpus": 20, "rxtx_factor": 10000, "disk": 32},
-               "OnMetal IO v1": {"ram": 131072, "vcpus": 40, "rxtx_factor": 10000, "disk": 32},
-               "OnMetal Memory v1": {"ram": 524288, "vcpus": 24, "rxtx_factor": 10000, "disk": 32}}
+    flavors = {"OnMetal Compute v1": {"id": "onmetal-compute1", "ram": 32768, "vcpus": 20,
+                                      "rxtx_factor": 10000, "disk": 32},
+               "OnMetal IO v1": {"id": "onmetal-io1", "ram": 131072, "vcpus": 40,
+                                 "rxtx_factor": 10000, "disk": 32},
+               "OnMetal Memory v1": {"id": "onmetal-memory1", "ram": 524288, "vcpus": 24,
+                                     "rxtx_factor": 10000, "disk": 32}}
 
     def extra_specs_json(self):
         """
@@ -204,10 +233,14 @@ class RackspacePerformance1Flavor(Flavor):
     """
     A Rackspace perfomance flavor object representation
     """
-    flavors = {"1 GB Performance": {"ram": 1024, "vcpus": 1, "rxtx_factor": 200, "disk": 20},
-               "2 GB Performance": {"ram": 2048, "vcpus": 2, "rxtx_factor": 400, "disk": 40},
-               "4 GB Performance": {"ram": 4096, "vcpus": 4, "rxtx_factor": 800, "disk": 40},
-               "8 GB Performance": {"ram": 8192, "vcpus": 8, "rxtx_factor": 1600, "disk": 40}}
+    flavors = {"1 GB Performance": {"id": "performance1-1", "ram": 1024, "vcpus": 1, "rxtx_factor": 200,
+                                    "disk": 20},
+               "2 GB Performance": {"id": "performance1-2", "ram": 2048, "vcpus": 2, "rxtx_factor": 400,
+                                    "disk": 40},
+               "4 GB Performance": {"id": "performance1-4", "ram": 4096, "vcpus": 4, "rxtx_factor": 800,
+                                    "disk": 40},
+               "8 GB Performance": {"id": "performance1-8", "ram": 8192, "vcpus": 8, "rxtx_factor": 1600,
+                                    "disk": 40}}
 
     def extra_specs_json(self):
         """
@@ -224,11 +257,16 @@ class RackspacePerformance2Flavor(Flavor):
     """
     A Rackspace performance flavor object representation
     """
-    flavors = {"15 GB Performance": {"ram": 15360, "vcpus": 4, "rxtx_factor": 1250, "disk": 40},
-               "30 GB Performance": {"ram": 30720, "vcpus": 8, "rxtx_factor": 2500, "disk": 40},
-               "60 GB Performance": {"ram": 61440, "vcpus": 16, "rxtx_factor": 5000, "disk": 40},
-               "90 GB Performance": {"ram": 92160, "vcpus": 24, "rxtx_factor": 7500, "disk": 40},
-               "120 GB Performance": {"ram": 122880, "vcpus": 32, "rxtx_factor": 10000, "disk": 40}}
+    flavors = {"15 GB Performance": {"id": "performance2-15", "ram": 15360, "vcpus": 4,
+                                     "rxtx_factor": 1250, "disk": 40},
+               "30 GB Performance": {"id": "performance2-30", "ram": 30720, "vcpus": 8,
+                                     "rxtx_factor": 2500, "disk": 40},
+               "60 GB Performance": {"id": "performance2-60", "ram": 61440, "vcpus": 16,
+                                     "rxtx_factor": 5000, "disk": 40},
+               "90 GB Performance": {"id": "performance2-90", "ram": 92160, "vcpus": 24,
+                                     "rxtx_factor": 7500, "disk": 40},
+               "120 GB Performance": {"id": "performance2-120", "ram": 122880, "vcpus": 32,
+                                      "rxtx_factor": 10000, "disk": 40}}
 
     def extra_specs_json(self):
         """
