59b67999c2d59ebca002f5bc12db2bd5453086d5
Tommy Walton <tommy.walton@RACKSPACE.COM>
2015-09-19 14:28:02 -0400

    move some tests

--- mimic/test/test_nova_images.py
+++ mimic/test/test_nova_images.py
@@ -4,7 +4,7 @@ Tests for :mod:`nova_api` and :mod:`nova_objects` for images.
 
 from twisted.trial.unittest import SynchronousTestCase
 
-from mimic.test.helpers import json_request
+from mimic.test.helpers import json_request, request
 from mimic.rest.nova_api import NovaApi
 from mimic.test.fixtures import APIMockHelper
 
@@ -114,3 +114,45 @@ class NovaAPIImagesTests(SynchronousTestCase):
         image_list = body['images']
         for each_image in image_list:
             self.assertEqual(sorted(each_image.keys()), sorted(['id', 'links', 'name']))
+
+    def test_get_image_list_after_list_images(self):
+        """
+        Test to verify :func:`get_image_list` on ``GET /v2.0/<tenant_id>/images``
+        """
+        image_list = self.get_server_image('/images')['images']
+        for each_image in image_list:
+            get_server_image_response_body = self.get_server_image(
+                '/images/' + each_image['id'])
+            self.assertEqual(each_image['id'],
+                             get_server_image_response_body['image']['id'])
+            self.assertEqual(each_image['name'],
+                             get_server_image_response_body['image']['name'])
+
+    def test_get_image_list_with_details_is_consistent(self):
+        """
+        Test to verify ``GET /v2.0/<tenant_id>/images/details`` returns
+        consistent data on mutiple list calls
+        """
+        get_image_list_response_body = self.get_server_image('/images/detail')
+        get_image_list2_response_body = self.get_server_image('/images/detail')
+        self.assertEqual(get_image_list_response_body, get_image_list2_response_body)
+
+    def test_get_server_image_negative(self):
+        """
+        Test to verify :func:`get_image` when invalid image from the
+        :obj: `mimic_presets` is provided.
+        """
+        get_server_image = request(self, self.root, "GET", self.uri +
+                                   '/images/1111')
+        get_server_image_response = self.successResultOf(get_server_image)
+        self.assertEqual(get_server_image_response.code, 404)
+
+    def test_get_server_image_negative2(self):
+        """
+        Test to verify :func:`get_image` when invalid image from the
+        :obj: `mimic_presets` is provided.
+        """
+        get_server_image = request(self, self.root, "GET", self.uri +
+                                   '/images/any-image-ending-with-Z')
+        get_server_image_response = self.successResultOf(get_server_image)
+        self.assertEqual(get_server_image_response.code, 404)
