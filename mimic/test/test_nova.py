2aa536b7cf98f1508a6817af5b9e8fddb6d21c82
Tommy Walton <tommy.walton@RACKSPACE.COM>
2015-09-23 18:36:42 -0400

    remove test that does not belong in test nova

--- mimic/test/test_nova.py
+++ mimic/test/test_nova.py
@@ -1667,24 +1667,6 @@ class NovaAPINegativeTests(SynchronousTestCase):
         get_server_response = self.successResultOf(get_server)
         self.assertEquals(get_server_response.code, 404)
 
-    def test_get_invalid_image(self):
-        """
-        Test to verify :func:`get_image` when invalid image from the
-        :obj: `mimic_presets` is provided or if image id ends with Z.
-        """
-        get_server_image = request(self, self.root, "GET", self.uri + '/images/image_ends_with_Z')
-        get_server_image_response = self.successResultOf(get_server_image)
-        self.assertEqual(get_server_image_response.code, 404)
-
-    def test_get_server_flavor(self):
-        """
-        Test to verify :func:`get_flavor` when invalid flavor from the
-        :obj: `mimic_presets` is provided.
-        """
-        get_server_flavor = request(self, self.root, "GET", self.uri + '/flavors/1')
-        get_server_flavor_response = self.successResultOf(get_server_flavor)
-        self.assertEqual(get_server_flavor_response.code, 404)
-
     def test_create_server_failure_using_behaviors(self):
         """
         :func:`create_server` fails with given error message and response code
