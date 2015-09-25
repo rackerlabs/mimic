0bcfeaf3d55220c5d15e62186eb5250af5203cab
Manish Tomar <manish.tomar@rackspace.com>
2015-09-25 11:17:52 -0700

    py6 lint

--- mimic/model/clb_errors.py
+++ mimic/model/clb_errors.py
@@ -133,4 +133,4 @@ def not_found_xml(item):
     return (
         '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
         '<itemNotFound xmlns="http://docs.openstack.org/loadbalancers/api/v1.0" code="404">'
-        '<message>{} not found</message></itemNotFound>').format(item)
+        '<message>{0} not found</message></itemNotFound>').format(item)
