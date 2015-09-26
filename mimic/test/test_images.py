0b215497728de0cd05137753905dd12abae5a6cd
Tommy Walton <tommy.walton@RACKSPACE.COM>
2015-09-25 23:59:36 -0400

    move all images tests to nova images and comment out tests in test_images

--- mimic/test/test_images.py
+++ mimic/test/test_images.py
@@ -1,113 +1,113 @@
-"""
-Tests for :mod:`nova_api` and :mod:`nova_objects` for images.
-"""
-
-import json
-from twisted.trial.unittest import SynchronousTestCase
-from mimic.test.helpers import json_request
-from mimic.rest.nova_api import NovaApi
-from mimic.test.fixtures import APIMockHelper
-
-
-class NovaAPIImagesTests(SynchronousTestCase):
-    """
-    Tests for images using the Nova Api plugin.
-    """
-
-    def setUp(self):
-        """
-        Create a :obj:`MimicCore` with :obj:`NovaApi` as the only plugin.
-        """
-        nova_api = NovaApi(['DFW'])
-        self.helper = APIMockHelper(self, [nova_api])
-        self.root = self.helper.root
-        self.uri = self.helper.uri
-
-    def get_server_image(self, postfix):
-        """
-        Get images, assert response code is 200 and return response body.
-        """
-        response, body = self.successResultOf(json_request(
-            self, self.root, "GET", self.uri + postfix))
-        self.assertEqual(200, response.code)
-        return body
-
-    def test_get_image_list(self):
-        """
-        Test to verify :func:`get_image_list` on ``GET /v2.0/<tenant_id>/images``
-        """
-        get_image_list_response_body = self.get_server_image('/images')
-        image_list = get_image_list_response_body['images']
-        self.assertEqual(len(image_list), 38)
-        for each_image in image_list:
-            self.assertEqual(sorted(each_image.keys()), sorted(['id', 'name', 'links']))
-
-    def test_get_image_list_with_details(self):
-        """
-        Test to verify :func:`get_image_list` on ``GET /v2.0/<tenant_id>/images/detail``
-        """
-        get_image_list_response_body = self.get_server_image('/images/detail')
-        image_list = get_image_list_response_body['images']
-        self.assertTrue(len(image_list) > 1)
-        for each_image in image_list:
-            self.assertEqual(sorted(each_image.keys()), sorted(['OS-EXT-IMG-SIZE:size', 'created',
-                                                                'progress', 'updated', 'status',
-                                                                'com.rackspace__1__ui_default_show',
-                                                                'id', 'links', 'metadata', 'minDisk',
-                                                                'minRam', 'name']))
-
-    def test_get_image_list_details_OnMetal(self):
-        """
-        Test to verify :func:`get_image_list` on ``GET /v2.0/<tenant_id>/images/detail``
-        includes OnMetal images in IAD
-        """
-        nova_api = NovaApi(['IAD'])
-        helper = APIMockHelper(self, [nova_api])
-        root = helper.root
-        uri = helper.uri
-        response, body = self.successResultOf(json_request(
-            self, root, "GET", uri + '/images/detail'))
-        self.assertEqual(200, response.code)
-        image_list = body['images']
-        self.assertEqual(len(body['images']), 52)
-        self.assertEquals(True, 'onmetal' in json.dumps(image_list))
-
-        for each_image in image_list:
-            self.assertEqual(sorted(each_image.keys()), sorted(['OS-EXT-IMG-SIZE:size', 'created',
-                                                                'progress', 'updated', 'status',
-                                                                'com.rackspace__1__ui_default_show',
-                                                                'id', 'links', 'metadata', 'minDisk',
-                                                                'minRam', 'name']))
-
-    def test_get_image_list_OnMetal(self):
-        """
-        Test to verify :func:`get_image_list` on ``GET /v2.0/<tenant_id>/images``
-        includes OnMetal images in IAD
-        """
-        nova_api = NovaApi(['IAD'])
-        helper = APIMockHelper(self, [nova_api])
-        root = helper.root
-        uri = helper.uri
-        response, body = self.successResultOf(json_request(
-            self, root, "GET", uri + '/images'))
-        self.assertEqual(200, response.code)
-        image_list = body['images']
-        self.assertEqual(len(image_list), 52)
-
-        for each_image in image_list:
-            self.assertEqual(sorted(each_image.keys()), sorted(['id', 'name', 'links']))
-
-    def test_get_image_that_does_not_exist(self):
-        """
-        Test to verify :func:`get_image` on ``GET /v2.0/<tenant_id>/images/<image_id>``
-        """
-        response, body = self.successResultOf(json_request(
-            self, self.root, "GET", self.uri + '/images/id_not_found'))
-
-        self.assertEqual(response.code, 404)
-        self.assertEqual(body, {
-            "itemNotFound": {
-                "message": "Image not found.",
-                "code": 404
-            }
-        })
+# """
+# Tests for :mod:`nova_api` and :mod:`nova_objects` for images.
+# """
+#
+# import json
+# from twisted.trial.unittest import SynchronousTestCase
+# from mimic.test.helpers import json_request
+# from mimic.rest.nova_api import NovaApi
+# from mimic.test.fixtures import APIMockHelper
+#
+#
+# class NovaAPIImagesTests(SynchronousTestCase):
+#     """
+#     Tests for images using the Nova Api plugin.
+#     """
+#
+#     def setUp(self):
+#         """
+#         Create a :obj:`MimicCore` with :obj:`NovaApi` as the only plugin.
+#         """
+#         nova_api = NovaApi(['DFW'])
+#         self.helper = APIMockHelper(self, [nova_api])
+#         self.root = self.helper.root
+#         self.uri = self.helper.uri
+#
+#     def get_server_image(self, postfix):
+#         """
+#         Get images, assert response code is 200 and return response body.
+#         """
+#         response, body = self.successResultOf(json_request(
+#             self, self.root, "GET", self.uri + postfix))
+#         self.assertEqual(200, response.code)
+#         return body
+#
+#     def test_get_image_list(self):
+#         """
+#         Test to verify :func:`get_image_list` on ``GET /v2.0/<tenant_id>/images``
+#         """
+#         get_image_list_response_body = self.get_server_image('/images')
+#         image_list = get_image_list_response_body['images']
+#         self.assertEqual(len(image_list), 38)
+#         for each_image in image_list:
+#             self.assertEqual(sorted(each_image.keys()), sorted(['id', 'name', 'links']))
+#
+#     def test_get_image_list_with_details(self):
+#         """
+#         Test to verify :func:`get_image_list` on ``GET /v2.0/<tenant_id>/images/detail``
+#         """
+#         get_image_list_response_body = self.get_server_image('/images/detail')
+#         image_list = get_image_list_response_body['images']
+#         self.assertTrue(len(image_list) > 1)
+#         for each_image in image_list:
+#             self.assertEqual(sorted(each_image.keys()), sorted(['OS-EXT-IMG-SIZE:size', 'created',
+#                                                                 'progress', 'updated', 'status',
+#                                                                 'com.rackspace__1__ui_default_show',
+#                                                                 'id', 'links', 'metadata', 'minDisk',
+#                                                                 'minRam', 'name']))
+#
+#     def test_get_image_list_details_OnMetal(self):
+#         """
+#         Test to verify :func:`get_image_list` on ``GET /v2.0/<tenant_id>/images/detail``
+#         includes OnMetal images in IAD
+#         """
+#         nova_api = NovaApi(['IAD'])
+#         helper = APIMockHelper(self, [nova_api])
+#         root = helper.root
+#         uri = helper.uri
+#         response, body = self.successResultOf(json_request(
+#             self, root, "GET", uri + '/images/detail'))
+#         self.assertEqual(200, response.code)
+#         image_list = body['images']
+#         self.assertEqual(len(body['images']), 52)
+#         self.assertEquals(True, 'onmetal' in json.dumps(image_list))
+#
+#         for each_image in image_list:
+#             self.assertEqual(sorted(each_image.keys()), sorted(['OS-EXT-IMG-SIZE:size', 'created',
+#                                                                 'progress', 'updated', 'status',
+#                                                                 'com.rackspace__1__ui_default_show',
+#                                                                 'id', 'links', 'metadata', 'minDisk',
+#                                                                 'minRam', 'name']))
+#
+#     def test_get_image_list_OnMetal(self):
+#         """
+#         Test to verify :func:`get_image_list` on ``GET /v2.0/<tenant_id>/images``
+#         includes OnMetal images in IAD
+#         """
+#         nova_api = NovaApi(['IAD'])
+#         helper = APIMockHelper(self, [nova_api])
+#         root = helper.root
+#         uri = helper.uri
+#         response, body = self.successResultOf(json_request(
+#             self, root, "GET", uri + '/images'))
+#         self.assertEqual(200, response.code)
+#         image_list = body['images']
+#         self.assertEqual(len(image_list), 52)
+#
+#         for each_image in image_list:
+#             self.assertEqual(sorted(each_image.keys()), sorted(['id', 'name', 'links']))
+#
+#     def test_get_image_that_does_not_exist(self):
+#         """
+#         Test to verify :func:`get_image` on ``GET /v2.0/<tenant_id>/images/<image_id>``
+#         """
+#         response, body = self.successResultOf(json_request(
+#             self, self.root, "GET", self.uri + '/images/id_not_found'))
+#
+#         self.assertEqual(response.code, 404)
+#         self.assertEqual(body, {
+#             "itemNotFound": {
+#                 "message": "Image not found.",
+#                 "code": 404
+#             }
+#         })
--- mimic/test/test_nova_images.py
+++ mimic/test/test_nova_images.py
@@ -7,19 +7,20 @@ from twisted.trial.unittest import SynchronousTestCase
 from mimic.test.helpers import json_request, request
 from mimic.rest.nova_api import NovaApi, NovaControlApi
 from mimic.test.fixtures import APIMockHelper
+import random
 
 
-class NovaAPIImagesTests(SynchronousTestCase):
+class NovaAPIVirtualImageTests(SynchronousTestCase):
 
     """
-    Tests for Images using the Nova Api plugin.
+    Tests for Virtual Images using the Nova Api plugin.
     """
 
     def setUp(self):
         """
         Create a :obj:`MimicCore` with :obj:`NovaApi` as the only plugin.
         """
-        nova_api = NovaApi(["ORD", "MIMIC"])
+        nova_api = NovaApi(["ORD"])
         self.helper = self.helper = APIMockHelper(
             self, [nova_api, NovaControlApi(nova_api=nova_api)]
         )
@@ -41,7 +42,7 @@ class NovaAPIImagesTests(SynchronousTestCase):
         :obj: `mimic_presets` is provided.
         """
         get_server_image = request(self, self.root, "GET", self.uri +
-                                   '/images/1111')
+                                   '/images/invalid_image_id')
         get_server_image_response = self.successResultOf(get_server_image)
         self.assertEqual(get_server_image_response.code, 404)
 
@@ -55,26 +56,42 @@ class NovaAPIImagesTests(SynchronousTestCase):
         get_server_image_response = self.successResultOf(get_server_image)
         self.assertEqual(get_server_image_response.code, 404)
 
+    def test_get_image_that_does_not_exist(self):
+        """
+        Test to verify :func:`get_image` on ``GET /v2.0/<tenant_id>/images/<image_id>``
+        """
+        response, body = self.successResultOf(json_request(
+            self, self.root, "GET", self.uri + '/images/id_not_found'))
+
+        self.assertEqual(response.code, 404)
+        self.assertEqual(body, {
+            "itemNotFound": {
+                "message": "Image not found.",
+                "code": 404
+            }
+        })
+
     def test_get_virtual_server_image(self):
         """
         Test to verify :func:`get_image` on ``GET /v2.0/<tenant_id>/images/<image_id>``
         """
         get_image_list_response_body = self.get_server_image('/images')
         image_list = get_image_list_response_body['images']
-        image_id = image_list[7]['id']
+        choice = random.randint(0, len(image_list) - 1)
+        image_id = image_list[choice]['id']
         get_image_response_body = self.get_server_image('/images/' + image_id)
         self.assertEqual(
             get_image_response_body['image']['id'], image_id)
         self.assertEqual(
             get_image_response_body['image']['status'], 'ACTIVE')
 
-    def test_get_image_list(self):
+    def test_get_virtual_server_image_list(self):
         """
         Test to verify :func:`get_image_list` on ``GET /v2.0/<tenant_id>/images``
         """
         get_image_list_response_body = self.get_server_image('/images')
         image_list = get_image_list_response_body['images']
-        self.assertTrue(len(image_list) > 1)
+        self.assertEqual(len(image_list), 38)
         for each_image in image_list:
             self.assertEqual(sorted(each_image.keys()), sorted(['id', 'name', 'links']))
 
@@ -115,33 +132,70 @@ class NovaAPIImagesTests(SynchronousTestCase):
             self.assertEqual(each_image['name'],
                              get_server_image_response_body['image']['name'])
 
+
+class NovaAPIOnMetalImageTests(SynchronousTestCase):
+
+    """
+    Tests for Virtual Images using the Nova Api plugin.
+    """
+
+    def setUp(self):
+        """
+        Create a :obj:`MimicCore` with :obj:`NovaApi` as the only plugin.
+        """
+        nova_api = NovaApi(["IAD"])
+        self.helper = self.helper = APIMockHelper(
+            self, [nova_api, NovaControlApi(nova_api=nova_api)]
+        )
+        self.root = self.helper.root
+        self.uri = self.helper.uri
+
+    def get_server_image(self, postfix):
+        """
+        Get images, assert response code is 200 and return response body.
+        """
+        (response, content) = self.successResultOf(json_request(
+            self, self.root, "GET", self.uri + postfix))
+        self.assertEqual(200, response.code)
+        return content
+
     def test_get_OnMetal_server_image(self):
         """
         Test to verify :func:`get_image` on ``GET /v2.0/<tenant_id>/images/<image_id>``
         """
-        helper = APIMockHelper(self, [NovaApi(['IAD'])])
-        root = helper.root
-        uri = helper.uri
-        response, body = self.successResultOf(json_request(
-            self, root, "GET", uri + '/images/detail'))
-        self.assertEqual(200, response.code)
-        images = body['images']
-        for image in images:
+        get_image_list_response_body = self.get_server_image('/images/detail')
+        image_list = get_image_list_response_body['images']
+        for image in image_list:
             if image['metadata']['flavor_classes'] == 'onmetal':
                 onmetal_id = image['id']
                 break
         response, body = self.successResultOf(json_request(
-            self, root, "GET", uri + '/images/' + onmetal_id))
+            self, self.root, "GET", self.uri + '/images/' + onmetal_id))
         self.assertEqual(200, response.code)
         self.assertEqual(body['image']['id'], onmetal_id)
         self.assertEqual(body['image']['metadata']['flavor_classes'], 'onmetal')
 
     def test_OnMetal_image_list(self):
-        helper = APIMockHelper(self, [NovaApi(['IAD'])])
-        root = helper.root
-        uri = helper.uri
-        response, body = self.successResultOf(json_request(self, root, "GET", uri + '/images'))
-        self.assertEqual(200, response.code)
-        image_list = body['images']
+        """
+        Test to verify :func:`images` on ``GET /v2.0/<tenant_id>/images``
+        """
+        get_image_list_response_body = self.get_server_image('/images')
+        image_list = get_image_list_response_body['images']
+        self.assertTrue(len(image_list), 52)
         for each_image in image_list:
             self.assertEqual(sorted(each_image.keys()), sorted(['id', 'links', 'name']))
+
+    def test_OnMetal_image_list_details(self):
+        """
+        Test to verify :func:`get_image_list` on ``GET /v2.0/<tenant_id>/images``
+        includes OnMetal images in IAD
+        """
+        get_image_list_response_body = self.get_server_image('/images/detail')
+        image_list = get_image_list_response_body['images']
+        self.assertTrue(len(image_list), 52)
+        for each_image in image_list:
+            self.assertEqual(sorted(each_image.keys()), sorted(['id', 'name', 'links', 'minRam',
+                                                                'status','OS-EXT-IMG-SIZE:size',
+                                                                'metadata', 'progress', 'created',
+                                                                'updated', 'minDisk',
+                                                                'com.rackspace__1__ui_default_show']))
