c7a14cb48f71ced1914b07bf1d0b5e504175af8a
Tommy Walton <tommy.walton@RACKSPACE.COM>
2015-10-06 17:17:20 -0400

    set Location header in create image responses

--- mimic/model/nova_objects.py
+++ mimic/model/nova_objects.py
@@ -954,6 +954,7 @@ class RegionalServerCollection(object):
                                               disk_config=disk_config)
 
             ImageStore.add_image_to_store(saved_image)
+            http_action_request.setHeader('Location', 'www.someurl.com')
             http_action_request.setResponseCode(202)
             return b''
 
