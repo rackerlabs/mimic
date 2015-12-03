db9316db0c895f8845f70ef12e3e2e050cbadd94
Glyph <glyph@twistedmatrix.com>
2015-12-02 17:23:58 -0800

    clean up the mess of confused python versions
    this will also hopefully speed up builds somewhat, since we won't be
    installing homebrew and pyenv unnecessarily.

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,15 +3,15 @@
 set -e
 set -x
 
-# Create an isolated virtualenv (travis's default is system-site-packages) for
-# tooling, so that it's all the right versions.
-virtualenv ~/.venv;
-source ~/.venv/bin/activate;
+if [[ "${MACAPP_ENV}" == "system" ]]; then
+    # If we are testing the mac app environment, it's a bit of a special case;
+    # the only setup we need to do is to ensure that homebrew python is the
+    # default.
 
-pip install --upgrade pip;
-pip install --upgrade pip;
-pip --version;
-pip install --upgrade --requirement=requirements/toolchain.txt;
+    brew update;
+    brew install python;
+    exit 0;
+fi;
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     DARWIN=true
@@ -19,57 +19,54 @@ else
     DARWIN=false
 fi
 
-if [[ "$DARWIN" = true ]]; then
+# Do we need to use a Python from pyenv, or will the system one suffice?
+PYENV_PYTHON="";
 
-    brew update
+if [[ "${TOXENV}" == "pypy" ]]; then
+    PYENV_PYTHON="pypy-4.0.1";
+fi;
 
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
+# If we need a python not available in Travis's environment, install pyenv in a
+# way appropriate to the platform.
+
+if [[ -n "${PYENV_PYTHON}" ]]; then
+    if [[ "$DARWIN" = true ]]; then
+        brew update;
+        brew install pyenv || brew upgrade pyenv;
+    else
+        git clone https://github.com/yyuu/pyenv.git ~/.pyenv;
+        PYENV_ROOT="$HOME/.pyenv";
+        PATH="$PYENV_ROOT/bin:$PATH";
     fi
+    eval "$(pyenv init -)";
+    pyenv install "${PYENV_PYTHON}";
+    pyenv global "${PYENV_PYTHON}";
+    pyenv rehash;
+fi;
 
-    case "${TOXENV}" in
-        py27)
-            brew upgrade pyenv
-            pyenv install 2.7.9
-            pyenv global 2.7.9
-            ;;
-        pypy)
-            brew upgrade pyenv
-            pyenv install pypy-2.6.1
-            pyenv global pypy-2.6.1
-            ;;
-    esac
-    pyenv rehash
-else
-    # temporary pyenv installation to get pypy-2.6 before container infra upgrade
-    if [[ "${TOXENV}" == "pypy" ]]; then
-        git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-        PYENV_ROOT="$HOME/.pyenv"
-        PATH="$PYENV_ROOT/bin:$PATH"
-        eval "$(pyenv init -)"
-        pyenv install pypy-2.6.1
-        pyenv global pypy-2.6.1
-    fi
-fi
+# Now that we know which Python we're using, create an isolated virtualenv
+# (travis's default is system-site-packages) for tooling, so that it's all the
+# right versions.
 
-if [[ "${MACAPP_ENV}" == "system" ]]; then
-    deactivate;
-    brew install python;
-else
-    # Codecov is special and must be allowed to float since it is a client for
-    # an online service, and changes actually do need to be synchronized with
-    # that service, unlike everything else which should be pinned to insulate
-    # against behavior changes.
-    pip install --upgrade --requirement=requirements/toolchain.txt codecov
-    coverage erase
-    tox --version;
-    env;
-    tox --recreate --notest
+python -m virtualenv ~/.venv;
+source ~/.venv/bin/activate;
 
-    # If "installdeps" fails, "tox" exits with an error, and the "set -e" above
-    # causes it to retry.  If "inst" fails, however, no error is reported for some
-    # reason.  The following line causes "grep" to exit with error (and thanks to
-    # "set -e", the whole script, so travis will retry it) if we didn't get to the
-    # end stage of "inst" (i.e. installing mimic itself).
-    ./.tox/"${TOXENV}"/bin/pip freeze | grep -e '^mimic=='
-fi
+pip install --upgrade pip;
+pip --version;
+# Codecov is special and must be allowed to float since it is a client for an
+# online service, and changes actually do need to be synchronized with that
+# service, unlike everything else which should be pinned to insulate against
+# behavior changes.
+pip install --upgrade --requirement=requirements/toolchain.txt codecov;
+hash -r;
+
+coverage erase;
+tox --version;
+tox --recreate --notest;
+
+# If "installdeps" fails, "tox" exits with an error, and the "set -e" above
+# causes it to retry.  If "inst" fails, however, no error is reported for some
+# reason.  The following line causes "grep" to exit with error (and thanks to
+# "set -e", the whole script, so travis will retry it) if we didn't get to the
+# end stage of "inst" (i.e. installing mimic itself).
+./.tox/"${TOXENV}"/bin/pip freeze | grep -e '^mimic==';
