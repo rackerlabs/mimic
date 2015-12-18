bd6620b3dd6576986177abb090317e27d5dea205
Glyph <glyph@twistedmatrix.com>
2015-12-17 17:34:31 -0800

    LAST MINUTE TWIST ENDING

--- setup.py
+++ setup.py
@@ -129,6 +129,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
