779e7e3820125f3caca8c37f0df71475777777a7
Glyph <glyph@twistedmatrix.com>
2016-01-27 16:28:22 -0800

    failing test for 404 case.

--- mimic/test/test_domain_mock.py
+++ mimic/test/test_domain_mock.py
@@ -1,5 +1,8 @@
 from twisted.internet.task import Clock
 from twisted.trial.unittest import SynchronousTestCase
+
+import treq
+
 from mimic.resource import MimicRoot
 from mimic.test.helpers import request, json_request
 from mimic.core import MimicCore
@@ -56,3 +59,21 @@ class TestDomainMock(SynchronousTestCase):
             "http://mybase/domain/api.example.com/"))
         self.assertEqual(200, response.code)
         self.assertEqual(content, u'test-value')
+
+    def test_domain_mock_no_child(self):
+        """
+        A GET on
+        ``http://mimic-host.example.com:port/domain/non-existent.example.com``
+        should return a 404 status assuming that there is no registered domain
+        mock with that name.
+        """
+        example_domain_api = ExampleDomainAPI()
+        core = MimicCore(Clock(), [], [example_domain_api])
+        root = MimicRoot(core).app.resource()
+
+        response = self.successResultOf(request(
+            self, root, b"GET",
+            b"http://mybase/domain/nope.example.com"))
+        self.assertEqual(404, response.code)
+        self.assertEqual(self.successResultOf(treq.content(response)),
+                         b"No such domain.")
