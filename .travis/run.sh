652d980fa9e84536af4c1fbcaabafa81022808ec
Glyph <glyph@twistedmatrix.com>
2016-01-30 03:32:16 -0800

    always get that var name wrong

--- .travis/run.sh
+++ .travis/run.sh
@@ -14,4 +14,4 @@ fi;
 
 source ~/.venv/bin/activate;
 
-PIP_WHEEL_DIR=".wheels" PIP_NO_INDEX=yes tox --recreate --develop -- $TOX_FLAGS;
+PIP_FIND_LINKS=".wheels" PIP_NO_INDEX=yes tox --recreate --develop -- $TOX_FLAGS;
