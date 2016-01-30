18d716dd6d3fa315a291e27b3759ee7b5b3cdf39
Glyph <glyph@twistedmatrix.com>
2016-01-30 04:22:21 -0800

    think this shell script couldn't get more horrible
    surprise!

--- .travis/install.sh
+++ .travis/install.sh
@@ -80,6 +80,12 @@ for req in requirements/*.txt; do
     ./.tox/"${TOXENV}"/bin/pip wheel -r "${req}" || true;
 done;
 
+# https://github.com/pypa/pip/issues/3414
+
+for fn in .wheels/*pp27*; do
+    mv -v "${fn}" "$(echo "${fn}" | sed -e s/pp27/pp240/g)";
+done;
+
 # If "installdeps" fails, "tox" exits with an error, and the "set -e" above
 # causes it to retry.  If "inst" fails, however, no error is reported for some
 # reason.  The following line causes "grep" to exit with error (and thanks to
